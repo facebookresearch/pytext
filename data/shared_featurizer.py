b14df45d38b4a21dd9e417a34a0e74e49818565a
Kushal Lakhotia <kushall@fb.com>
2018-08-20 19:03:18 -0700

    Add support for multiple tokenizers in Featurizer
    Summary: Featurizer currently supports only 1 tokenizer. Add support for multiple tokenizers. For multi-lingual scenario tokenizers for > 1 language can be configured in Featurizer.
    
    Reviewed By: sebschu
    
    Differential Revision: D9183221
    
    fbshipit-source-id: fef5589405fd457146c2cea41de62dd0aa878359

--- data/shared_featurizer.py
+++ data/shared_featurizer.py
@@ -1,43 +1,57 @@
 #!/usr/bin/env python3
-from libfb.py.thrift_clients.configerator_thrift_client import ConfigeratorThriftClient
-from thrift.util import Serializer
-from assistant.lib.feat.ttypes import ModelFeatures
-from facebook.assistant.lib.featurization_lib import FeaturizationWrapper
-from thrift.protocol import TSimpleJSONProtocol, TBinaryProtocol
-from pytext.common.constants import ConfigeratorPath
-from typing import List, Tuple
+from typing import Dict, List, Tuple
 
-json_factory = TSimpleJSONProtocol.TSimpleJSONProtocolFactory()
-bin_factory = TBinaryProtocol.TBinaryProtocolFactory()
+from assistant.lib.feat.ttypes import ModelFeatures
+from facebook.assistant.lib.featurization_lib import (
+    DEFAULT_LOCALE,
+    FeaturizationWrapper,
+)
 from joblib import Parallel, delayed
+from libfb.py.thrift_clients.configerator_thrift_client import ConfigeratorThriftClient
+from pytext.common.constants import ConfigeratorPath
+from thrift.protocol import TBinaryProtocol
+from thrift.util import Serializer
+
+
+BIN_FACTORY = TBinaryProtocol.TBinaryProtocolFactory()
+DEFAULT_TOKENIZER_CONFIG = {DEFAULT_LOCALE: ConfigeratorPath.DEFAULT_TOKENIZER}
 
 
 class SharedFeaturizer(object):
-    def __init__(self, config_path: str = ConfigeratorPath.DEFAULT_TOKENIZER) -> None:
+    def __init__(
+        self, tokenizer_config_path_dict: Dict = DEFAULT_TOKENIZER_CONFIG
+    ) -> None:
+        tokenizer_configs: dict = {}
         with ConfigeratorThriftClient() as ctc:
-            tokenizer_config = ctc.getConfigContents(config_path)
-            self.featurizer = FeaturizationWrapper(tokenizer_config)
+            for name, config_path in tokenizer_config_path_dict.items():
+                tokenizer_configs[name] = ctc.getConfigContents(config_path)
+        self.featurizer = FeaturizationWrapper(tokenizer_configs)
 
     # raw_dict is expected to be serialized version of
     # caffe2.fb.text.TokenSparseFeatsList object
-    def featurize(self, raw_text: str, raw_dict: str) -> ModelFeatures:
-        features_bin = self.featurizer.featurize_raw_text(raw_text, raw_dict)
+    def featurize(
+        self, raw_text: str, raw_dict: str, locale: str = ""
+    ) -> ModelFeatures:
+        features_bin = self.featurizer.featurize_raw_text(raw_text, raw_dict, locale)
         # Get ModelFeatures object
         features_obj = Serializer.deserialize(
-            bin_factory, features_bin, ModelFeatures()
+            BIN_FACTORY, features_bin, ModelFeatures()
         )
 
         return features_obj
 
     def featurize_parallel(
-        self, raw_feats: List[Tuple[str, str]], num_threads: int
+        self, raw_feats: List[Tuple[str, ...]], num_threads: int
     ) -> List[ModelFeatures]:
+        if len(raw_feats) > 0 and len(raw_feats[0]) == 2:
+            # Set locale to empty string so that default locale is used.
+            raw_feats = list(map(lambda tup: (tup[0], tup[1], ""), raw_feats))
         features_bin_list = self.featurizer.featurize_batch_parallel(
             raw_feats, num_threads
         )
         # Deserialize ModelFeatures objects
         features_obj_list = Parallel(n_jobs=num_threads)(
-            delayed(Serializer.deserialize)(bin_factory, features_bin, ModelFeatures())
+            delayed(Serializer.deserialize)(BIN_FACTORY, features_bin, ModelFeatures())
             for features_bin in features_bin_list
         )
         return features_obj_list
