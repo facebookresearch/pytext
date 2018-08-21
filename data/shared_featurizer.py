a3ba0233597f1ede7e9788d0513d9be8af67d2c1
Kushal Lakhotia <kushall@fb.com>
2018-08-21 14:41:19 -0700

    Add sentence marker support to SharedFeaturizer
    Summary: ^ same as description
    
    Reviewed By: abhinavarora
    
    Differential Revision: D9437785
    
    fbshipit-source-id: 1f67539bbd46716fed251e7fa6d4a03f2f076ce2

--- data/shared_featurizer.py
+++ data/shared_featurizer.py
@@ -18,20 +18,34 @@ DEFAULT_TOKENIZER_CONFIG = {DEFAULT_LOCALE: ConfigeratorPath.DEFAULT_TOKENIZER}
 
 
 class SharedFeaturizer(object):
+    """
+    This is a wrapper class over Python binding for Featurizer and, serves as
+    an interface for data handler for preprocessing raw input data.
+    This interface is specific for Assistant NLU's use-case for now.
+    """
+
     def __init__(
-        self, tokenizer_config_path_dict: Dict = DEFAULT_TOKENIZER_CONFIG
+        self,
+        tokenizer_config_path_dict: Dict[str, str] = DEFAULT_TOKENIZER_CONFIG,
+        sentence_markers_dict: Dict[str, Tuple[str, str]] = None,
     ) -> None:
-        tokenizer_configs: dict = {}
+        tokenizer_config_dict: dict = {}
         with ConfigeratorThriftClient() as ctc:
             for name, config_path in tokenizer_config_path_dict.items():
-                tokenizer_configs[name] = ctc.getConfigContents(config_path)
-        self.featurizer = FeaturizationWrapper(tokenizer_configs)
+                tokenizer_config_dict[name] = ctc.getConfigContents(config_path)
+        if sentence_markers_dict is None:
+            self.featurizer = FeaturizationWrapper(tokenizer_config_dict)
+        else:
+            self.featurizer = FeaturizationWrapper(
+                tokenizer_config_dict, sentence_markers_dict
+            )
 
     # raw_dict is expected to be serialized version of
     # caffe2.fb.text.TokenSparseFeatsList object
     def featurize(
         self, raw_text: str, raw_dict: str, locale: str = ""
     ) -> ModelFeatures:
+        """Featurize one instance/example only."""
         features_bin = self.featurizer.featurize_raw_text(raw_text, raw_dict, locale)
         # Get ModelFeatures object
         features_obj = Serializer.deserialize(
@@ -43,6 +57,7 @@ class SharedFeaturizer(object):
     def featurize_parallel(
         self, raw_feats: List[Tuple[str, ...]], num_threads: int
     ) -> List[ModelFeatures]:
+        """Featurize a batch of instances/examples parallelly."""
         if len(raw_feats) > 0 and len(raw_feats[0]) == 2:
             # Set locale to empty string so that default locale is used.
             raw_feats = list(map(lambda tup: (tup[0], tup[1], ""), raw_feats))
