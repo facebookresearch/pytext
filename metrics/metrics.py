fix sqrt in mcc calculation
Summary:
for ints too big np.sqrt defaults to calling object sqrt.
See more detailed explanation here https://stackoverflow.com/questions/47123035/python-error-when-calling-numpy-from-class-method-with-map

converting to float resolves it

Reviewed By: vesko-st, liaimi

Differential Revision: D10122150

fbshipit-source-id: 9bce88d813c742d5b9db25b2141499cb877a3490

metrics/metrics.py
@@ -665,7 +665,8 @@ def compute_matthews_correlation_coefficients(
     """
 
     mcc = _safe_division(
-        (TP * TN) - (FP * FN), np.sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN))
+        (TP * TN) - (FP * FN),
+        np.sqrt(float((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN))),
     )
     return mcc
 