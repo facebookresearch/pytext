b3542030561914fabe71ffce1da115bb3f7fa14e
Eric Gaudet <egaudet@fb.com>
2018-11-09 16:21:38 -0800

    experiment circleci configs 3/x
    Summary:
    - fix config.yml
    - add protobuf to compile onnx
    - quiet progress bar for circleci logs
    
    (Note: this ignores all push blocking failures!)
    
    Reviewed By: ahhegazy
    
    Differential Revision: D13011812
    
    fbshipit-source-id: 3076bd22eb2040dac48b32afac82315a5b42fb0b

--- .circleci/setup_circleimg.sh
+++ .circleci/setup_circleimg.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 sudo apt-get update
-sudo apt-get install -y cmake python-pip python-dev build-essential
+sudo apt-get install -y cmake python-pip python-dev build-essential protobuf-compiler libprotoc-dev
 sudo ./install_deps
