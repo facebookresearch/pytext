fix max_num_batches calculation (#10)
Summary:
Pull Request resolved: https://github.com/facebookresearch/pytext/pull/10

In distributed training, we have the logic that each shard will keep yield the results until it reach the max_num_batches.
But if the max_num_batches calculation generate the wrong result, which cause the last shard have more batches than the max_num_batches,
the last process will stuck on the grads sync.

Here is an example P60390119

```
Give len(dataset) = 122, world_size = 8, batch_size = 64

num_examples_per_device ==> [15, 15, 15, 15, 15, 15, 15, 17]

batch_size = batch_size // world_size ==> 64 // 8 = 8
num_all_batches = math.ceil(len(train_dataset) / float(batch_size)) ==> ceil(122 / 8.0) = 16
total_num_batches = math.ceil(num_all_batches / float(world_size)) ==> ceil(16 / 8.0) = 2

We pass total_num_batches = 2 in all BatchIterator, but the last shard have 17 examples, which batch_size is 3

Differential Revision: D13311684

fbshipit-source-id: bd60031d005f7db7a3c6410a0d801d4772017c92

pytext/trainers/trainer.py
@@ -116,11 +116,17 @@ class Trainer(TrainerBase):
             loss.backward()
             if scheduler:
                 scheduler.step_batch()
+
             if self.config.max_clip_norm is not None:
-                torch.nn.utils.clip_grad_norm_(
+                grad_norm = torch.nn.utils.clip_grad_norm_(
                     model.parameters(), self.config.max_clip_norm
                 )
+            else:
+                grad_norm = None
+
             optimizer_step(optimizers)
+            # grad_norm could be used to check grads sync in distributed training
+            return grad_norm
 
         for epoch in range(1, self.config.epochs + 1):
             print(f"Rank {rank} worker: Starting epoch #{epoch}")