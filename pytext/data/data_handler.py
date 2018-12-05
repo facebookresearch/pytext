ea1e260fec2efbe6b7fcf406757c1662581a06bd
Shicong Zhao <zsc@fb.com>
2018-12-05 12:36:21 -0800

    Datahandler docstring
    Summary: as title
    
    Differential Revision: D13292920
    
    fbshipit-source-id: fd13cfe14c7356c1226b89d93af831f3b286fce4

--- pytext/data/data_handler.py
+++ pytext/data/data_handler.py
@@ -25,10 +25,25 @@ class CommonMetadata:
 
 class BatchIterator:
     """
-    Each iteration will return a tuple of (input, target, context)
-    input can be feeded directly into model forward function
-    target is the numaricalized label(s)
-    context is any extra info to be used in downstream steps
+    BatchIterator is a wrapper of TorchText. Iterator that provide flexiblity to
+    map batched data to a tuple of (input, target, context) and other additional
+    steps such as dealing with distributed training.
+
+    Args:
+        batches (Iterator[TorchText.Batch]): iterator of TorchText.Batch, which
+            shuffles/batches the data in __iter__ and return a batch of data in
+            __next__
+        processor: function to run after getting batched data from TorchText.Iterator,
+            the function should define a way to map to data into
+            (input, target, context)
+        include_input (bool): if input data should be returned, default is true
+        include_target (bool): if target data should be returned, default is true
+        include_context (bool): if context data should be returned, default is true
+        is_train (bool): if the batch data is for training
+        num_batches (int): total batches to generate, this param if for distributed
+            training due to a limitation in PyTorch's distributed training backend
+            that enforces all the parallel workers to have the same number of batches
+            we workaround it by adding dummy batches at the end
     """
 
     def __init__(
@@ -50,6 +65,18 @@ class BatchIterator:
         self.total_num_batches = num_batches
 
     def __iter__(self):
+        """
+        Iterate Torchtext.Iterator, map batch data into (input, target, context)
+        tuple and generate dummy batches for distributed training
+
+        Returns:
+            input: tuple of tensors that can be feeded directly into model forward
+                function
+            target: tensor or tuple of tensors as the model target for computing
+                loss
+            context: any extra info to be used in downstream steps, can be any
+                data type
+        """
         num_batches = len(self.batches)
         for i, batch in enumerate(self.batches):
             input, target, context = self.processor(
@@ -60,7 +87,7 @@ class BatchIterator:
                 self.is_train,
             )
             yield (input, target, context)
-            # Due to a limitation in PyTroch's distributed training backend that
+            # Due to a limitation in PyTorch's distributed training backend that
             # enforces that all the parallel workers to have the same number of
             # batches we keep yielding the last batch until the requested total
             # number of batches is fullfilled
@@ -72,21 +99,56 @@ class BatchIterator:
 
 
 class DataHandler(Component):
-    """ Defines a pipeline to process data and generate tensors to be consumed by model,
-        1 construct the DataHandler object
-        2 call init_vocab to build necessary vocabs
-        3 call get_meta to get meta data of all fields
-        4 call batch to get batch iterator (check gen_dataset
-          function to understand the details of the process pipeline)
-        5 each batch is a (input, target, context) tuple, in which input can be feed
-          directly into model.
+    """
+    DataHandler is the central place to prepare data for model training/testing.
+    The class is responsible of:
+        1 Define pipeline to process data and generate batch of tensors to be
+        consumed by model. Each batch is a (input, target, extra_data) tuple, in
+        which input can be feed directly into model.
+        2 Initilize global context, such as build vocab, load pretrained embeddings.
+        Store the context as metadata, and provide function to serialize/deserialize
+        the metadata
+
+    The data processing pipeline contains the following steps:
+        1 Read data from file into a list of raw data examples
+        2 Convert each row of row data to a TorchText Example. This logic happens
+        in process_row function and will:
+            2.1 Invoke featurizer, which contains data processing steps to apply
+            for both training and inference time, e.g: tokenization
+            2.2 Use the raw data and results from featurizer to do any prepross
+        3 Generate a TorchText.Dataset that contains the list of Example, the Dataset
+        also has a list of TorchText.Field, which defines how to do padding and
+        numericalization while batching data.
+        4 Return a BatchIterator which will give a tuple of (input, target, context)
+        tensors for each iteration. By default the tensors have a 1:1 mapping to
+        the TorchText.Field fields, but this behavior can be overwritten by
+        _input_from_batch, _target_from_batch, _context_from_batch functions.
 
     Attributes:
-        raw_columns: columns to read from data source. In case of files, the order
-            should match the data stored in that file
-        labels:
-        features:
-        extra_fields
+        raw_columns (List[str]): columns to read from data source. The order should
+            match the data stored in that file.
+        featurizer (Featurizer): performe data preprocessing that should be shared
+            between training and inference
+        features (Dict[str, Field]): a dict of name -> field that used to process data
+            as model input
+        labels (Dict[str, Field]): a dict of name -> field that used to process data
+            as training target
+        extra_fields (Dict[str, Field]): fields that process any extra data used
+            neither as model input nor target. This is None by default
+        text_feature_name (str): name of the text field, used to define the default
+            sort key of data
+        shuffle (bool): if the dataset should be shuffled, true by default
+        sort_within_batch (bool): if data within same batch should be sorted, true
+            by default
+        train_path (str): path of training data file
+        eval_path (str): path of evaluation data file
+        test_path (str): path of test data file
+        train_batch_size (int): training batch size, 128 by default
+        eval_batch_size (int): evaluation batch size, 128 by default
+        test_batch_size (int): test batch size, 128 by default
+        max_seq_len (int): maximum length of tokens to keep in sequence
+        pass_index (bool): if the orignal index of data in the batch should be
+            passed along to downstream steps, default is true
     """
 
     class Config(ConfigBase):
@@ -152,6 +214,12 @@ class DataHandler(Component):
         Items are added to the set from top of the file to bottom.
         So, the items in the file should be ordered by a preference (if any), e.g.,
         it makes sense to order tokens in descending order of frequency in corpus.
+
+        Args:
+            vocab_file (str): vocab file to load
+            vocab_size (int): maximum tokens to load, will only load the first n if
+                the acutal vocab size is larger than this parameter
+            lowercase_tokens (bool): if the tokens should be lowercased
         """
         vocab: Set[str] = set()
         if os.path.isfile(vocab_file):
@@ -170,10 +238,19 @@ class DataHandler(Component):
             print(f"{vocab_file} doesn't exist. Cannot load vocabulary from it")
         return vocab
 
-    def sort_key(self, ex: textdata.Example) -> Any:
-        return len(getattr(ex, self.text_feature_name))
+    def sort_key(self, example: textdata.Example) -> Any:
+        """
+        How to sort data in every batch, default behavior is by the length of input
+        text
+        Args:
+            example (Example): one torchtext example
+        """
+        return len(getattr(example, self.text_feature_name))
 
     def metadata_to_save(self):
+        """
+        Save metadata, pretrained_embeds_weight should be excluded
+        """
         # make a copy
         metadata = deepcopy(self.metadata)
         # pretrained_embeds_weight takes a lot space and is not needed in inference time
@@ -182,6 +259,9 @@ class DataHandler(Component):
         return metadata
 
     def load_metadata(self, metadata: CommonMetadata):
+        """
+        Load previously saved metadata
+        """
         self.metadata = metadata
         for name, field in self.features.items():
             if field.use_vocab and name in metadata.features:
@@ -196,6 +276,11 @@ class DataHandler(Component):
     def gen_dataset_from_path(
         self, path: str, include_label_fields: bool = True, use_cache: bool = True
     ) -> textdata.Dataset:
+        """
+        Generate a dataset from file
+        Returns:
+            dataset (TorchText.Dataset)
+        """
         if use_cache and path in self._data_cache:
             return self._data_cache[path]
         res = self.gen_dataset(
@@ -207,7 +292,10 @@ class DataHandler(Component):
     def gen_dataset(
         self, data: List[Dict[str, Any]], include_label_fields: bool = True
     ) -> textdata.Dataset:
-        """ generate torchtext Dataset from dataframe.
+        """
+        Generate torchtext Dataset from raw in memory data.
+        Returns:
+            dataset (TorchText.Dataset)
         """
         to_process = {}
         to_process.update(self.features)
@@ -222,6 +310,12 @@ class DataHandler(Component):
         return textdata.Dataset(examples, to_process)
 
     def preprocess(self, data: List[Dict[str, Any]]):
+        """
+        preprocess the raw data to create TorchText.Example, this is the second
+        step in whole processing pipeline
+        Returns:
+            data (Generator[Dict[str, Any]])
+        """
         for idx, row in enumerate(data):
             preprocessed_row = self.preprocess_row(row)
             if preprocessed_row:
@@ -230,11 +324,14 @@ class DataHandler(Component):
 
     def preprocess_row(self, row_data: Dict[str, Any]) -> Dict[str, Any]:
         """
-        preprocess steps for a single input row
+        preprocess steps for a single input row, sub class should override it
         """
         return row_data
 
     def init_metadata_from_path(self, train_path, eval_path, test_path):
+        """
+        Initilize metadata using data from file
+        """
         # get data sets
         self._init_metadata(
             *[
@@ -244,9 +341,15 @@ class DataHandler(Component):
         )
 
     def init_metadata(self):
+        """
+        Initilize metadata using data from configured path
+        """
         self.init_metadata_from_path(self.train_path, self.eval_path, self.test_path)
 
     def init_metadata_from_raw_data(self, *data):
+        """
+        Initilize metadata using in memory data
+        """
         self._init_metadata(*[self.gen_dataset(d) for d in data])
 
     def _init_metadata(
@@ -372,6 +475,17 @@ class DataHandler(Component):
     def get_train_iter_from_path(
         self, train_path: str, batch_size: int, rank: int = 0, world_size: int = 1
     ) -> BatchIterator:
+        """
+        Generate data batch iterator for training data. See `_get_train_iter()` for
+        details
+
+        Args:
+            train_path (str): file path of training data
+            batch_size (int): batch size
+            rank (int): used for distributed training, the rank of current Gpu,
+                don't set it to anything but 0 for non-distributed training
+            world_size (int): used for distributed training, total number of Gpu
+        """
         return self._get_train_iter(
             self.gen_dataset_from_path(train_path), batch_size, rank, world_size
         )
@@ -417,6 +531,19 @@ class DataHandler(Component):
         rank: int = 0,
         world_size: int = 1,
     ) -> BatchIterator:
+        """
+        Generate data batch iterator for training data. If distributed training
+        is enabled, the dataset will be partitioned first. We use BucketIterator
+        here to pool together examples with a similar size length to reduce the
+        padding required for each batch.
+
+        Args:
+            train_dataset (str): training dataset
+            batch_size (int): batch size
+            rank (int): used for distributed training, the rank of current Gpu,
+                don't set it to anything but 0 for non-distributed training
+            world_size (int): used for distributed training, total number of Gpu
+        """
         dataset_shard, max_num_examples = self._get_dataset_shard(
             train_dataset, rank, world_size
         )
@@ -504,8 +631,14 @@ class DataHandler(Component):
     def read_from_file(
         file_name: str, columns_to_use: Union[Dict[str, int], List[str]]
     ) -> List[Dict[str, Any]]:
-        """ Read data from csv file. Input file format is required to be
+        """
+        Read data from csv file. Input file format is required to be
         tab-separated columns
+
+        Args:
+            file_name (str): csv file name
+            columns_to_use (Union[Dict[str, int], List[str]]): either a list of
+                column names or a dict of column name -> column index in the file
         """
         print("reading data from {}".format(file_name))
         if isinstance(columns_to_use, list):
