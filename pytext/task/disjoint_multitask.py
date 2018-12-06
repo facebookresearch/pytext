1bf7001632f648dd3d0d7377f0a2d7d750c4e17d
Shicong Zhao <zsc@fb.com>
2018-12-06 12:26:59 -0800

    copyright headers
    Summary: on newly added py files and .bat file
    
    Differential Revision: D13363894
    
    fbshipit-source-id: 253facd6bf6c9c0f6920f2539b3e7e2f86b15442

--- pytext/task/disjoint_multitask.py
+++ pytext/task/disjoint_multitask.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from collections import OrderedDict
 from pprint import pprint
 from typing import Dict, Union
