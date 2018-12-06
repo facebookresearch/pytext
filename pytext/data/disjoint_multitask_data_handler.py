8afbc773f4cd2ca8cb9adbf1bad9f5d3884d5810
Shicong Zhao <zsc@fb.com>
2018-12-06 12:39:24 -0800

    copyright headers (#30)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/30
    
    on newly added py files and .bat file
    
    Reviewed By: ahhegazy
    
    Differential Revision: D13363894
    
    fbshipit-source-id: 603cffab1f8e15563b207c796bcda197c5b7b830

--- pytext/data/disjoint_multitask_data_handler.py
+++ pytext/data/disjoint_multitask_data_handler.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 import itertools
 from collections import OrderedDict
 from typing import Dict, Optional, Tuple
