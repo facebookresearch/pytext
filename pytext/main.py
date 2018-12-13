aa3111e20fa051ec7f7734f25310648573d7ae1d
Eric Gaudet <egaudet@fb.com>
2018-12-12 17:46:02 -0800

    gen_default_config command
    Summary:
    This diff introduces a command to generate a full config with default
    values. Users can start with the generated config as a template to get
    started, or to discover useful parameters they can tweak.
    
    Introduces a unit test to try and gen all the Task compononents, except the
    ones that don't have a default value (to be fixed in later diffs)
    
    Differential Revision: D13248917
    
    fbshipit-source-id: a983e391d2190b119d49e9e1df5339637e2ed6f3

--- pytext/main.py
+++ pytext/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
+import enum
 import json
 import pprint
 import sys
@@ -10,12 +11,14 @@ import click
 import torch
 from pytext import create_predictor
 from pytext.config import PyTextConfig, TestConfig
-from pytext.config.serialize import Mode, config_from_json, config_to_json, parse_config
+from pytext.config.serialize import config_from_json, config_to_json
 from pytext.task import load
 from pytext.utils.documentation_helper import (
     ROOT_CONFIG,
+    eprint,
     find_config_class,
     pretty_print_config_class,
+    replace_components,
 )
 from pytext.workflow import (
     batch_predict,
@@ -26,9 +29,26 @@ from pytext.workflow import (
 from torch.multiprocessing.spawn import spawn
 
 
+class Mode(enum.Enum):
+    TRAIN = "train"
+    TEST = "test"
+
+
 class Attrs:
     def __repr__(self):
         return f"Attrs({', '.join(f'{k}={v}' for k, v in vars(self).items())})"
+
+
+def parse_config(mode, config_json):
+    """
+    Parse PyTextConfig object from parameter string or parameter file
+    """
+    config_cls = {Mode.TRAIN: PyTextConfig, Mode.TEST: TestConfig}[mode]
+    # TODO T32608471 should assume the entire json is PyTextConfig later, right
+    # now we're matching the file format for pytext trainer.py inside fbl
+    if "config" not in config_json:
+        return config_from_json(config_cls, config_json)
+    return config_from_json(config_cls, config_json["config"])
 
 
 def train_model_distributed(config):
@@ -71,6 +91,39 @@ def train_model_distributed(config):
 def run_single(rank, config_json: str, world_size: int, dist_init_method: str):
     config = config_from_json(PyTextConfig, config_json)
     train_model(config, dist_init_method, rank, rank, world_size)
+
+
+def gen_config_impl(task_name, options):
+    task_class_set = find_config_class(task_name)
+    if not task_class_set:
+        raise Exception(f"Unknown task class: {task_name}")
+    elif len(task_class_set) > 1:
+        raise Exception(f"Multiple tasks named {task_name}: {task_class_set}")
+
+    task_class = next(iter(task_class_set))
+    root = PyTextConfig(task=task_class.Config())
+
+    # Use components listed in options instead of defaults
+    for opt in options:
+        replace_class_set = find_config_class(opt)
+        if not replace_class_set:
+            raise Exception(f"Not a component class: {opt}")
+        elif len(replace_class_set) > 1:
+            raise Exception(f"Multiple component named {opt}: {replace_class_set}")
+        replace_class = next(iter(replace_class_set))
+        found = replace_components(root, opt, set(replace_class.__bases__))
+        if found:
+            eprint("INFO - Applying option:", "->".join(reversed(found)), "=", opt)
+            obj = root
+            for k in reversed(found[1:]):
+                obj = getattr(obj, k)
+            if hasattr(replace_class, "Config"):
+                setattr(obj, found[0], replace_class.Config())
+            else:
+                setattr(obj, found[0], replace_class())
+        else:
+            raise Exception(f"Unknown option: {opt}")
+    return config_to_json(PyTextConfig, root)
 
 
 @click.group()
@@ -120,6 +173,28 @@ def help_config(context, class_name):
             print()
     else:
         raise Exception(f"Unknown component name: {class_name}")
+
+
+@main.command(help="Generate a config JSON file with default values.")
+@click.argument("task_name")
+@click.argument("options", nargs=-1)
+@click.pass_context
+def gen_default_config(context, task_name, options):
+    """
+        Generate a config for `task_name` with default values.
+        Optionally, override the defaults by passing your desired
+        components as `options`.
+    """
+    try:
+        cfg = gen_config_impl(task_name, options)
+    except TypeError as ex:
+        eprint(
+            "ERROR - Cannot create this config",
+            "because some fields don't have a default value:",
+            ex,
+        )
+        sys.exit(-1)
+    print(json.dumps(cfg, sort_keys=True, indent=2))
 
 
 @main.command()
