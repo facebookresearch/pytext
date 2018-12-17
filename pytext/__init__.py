78c10a2ffd008c658742951a8ee81df47a55dd5a
Haoran Li <aimeeli@fb.com>
2018-12-16 18:34:03 -0800

    elmo benchmark
    Summary: This script runs evaluation on a predictor model on given test data, prints evaluation metrics and latency
    
    Reviewed By: jingfeidu
    
    Differential Revision: D13318146
    
    fbshipit-source-id: 2208e4e37f133e0e17a81d8f052ede0814637657

--- pytext/__init__.py
+++ pytext/__init__.py
@@ -4,6 +4,7 @@ import json
 import uuid
 from typing import Callable, Mapping, Optional
 
+import libfb.py.mkl  # noqa
 import numpy as np
 from caffe2.python import workspace
 from caffe2.python.predictor import predictor_exporter
