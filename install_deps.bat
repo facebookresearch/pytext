3ef41fb78cd23bc3317e53f694f7c07215763538
Eric Gaudet <egaudet@fb.com>
2019-01-22 16:53:07 -0800

    EASY [PyText] use pip 18.1 to allow circleci tests to run (#232)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/232
    
    see task
    
    Reviewed By: hikushalhere
    
    Differential Revision: D13777321
    
    fbshipit-source-id: 5f1f8e60765acf7ac9a578031e39fd0eeb47925c

--- install_deps.bat
+++ install_deps.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 ::Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-python -m pip install --upgrade pip
+python -m pip install --upgrade pip==18.1
 pip install -e . --process-dependency-links --no-cache-dir --progress-bar off
