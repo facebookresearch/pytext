ce2d4c2b688a6dd08d617f0b00ba0fbff19e380a
Armen Aghajanyan <armenag@fb.com>
2019-02-21 17:21:15 -0800

    update ONNX to 1.4.1
    Summary: Pull Request resolved: https://github.com/facebookresearch/pytext/pull/321
    
    Differential Revision: D14175788
    
    fbshipit-source-id: 22647852f16900202aeb1fc6d52d75316986dee3

--- .circleci/setup_circleimg.sh
+++ .circleci/setup_circleimg.sh
@@ -2,5 +2,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 sudo apt-get update
 sudo apt-get install -y cmake python-pip python-dev build-essential protobuf-compiler libprotoc-dev
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 800 --slave /usr/bin/g++ g++ /usr/bin/g++-8
 sudo ./install_deps
 sudo pip install --progress-bar off pytest pytest-cov
