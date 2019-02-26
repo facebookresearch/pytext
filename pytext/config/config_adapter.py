3d5c8fe7aa6b89beb5cf35961f920e62b729dfdd
Arun Babu <arbabu@fb.com>
2019-02-26 14:23:36 -0800

    Adapter for scheduler change (#328)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/328
    
    Adapter to handle the breaking changes as the part of scheduler refactoring.
    
    Differential Revision: D14194075
    
    fbshipit-source-id: e681b3de2d9a436fcdc7b0e80bdce081f2c48d98

--- pytext/config/config_adapter.py
+++ pytext/config/config_adapter.py
@@ -36,7 +36,72 @@ def v0_to_v1(json_config):
     return json_config
 
 
-adapters = {0: v0_to_v1}
+def v1_to_v2(json_config):
+    # migrate optimizer params
+    [task] = json_config["task"].values()
+    if (
+        "scheduler" not in task
+        or task["scheduler"] is None
+        or "LmFineTuning" in task["scheduler"]
+        or "StepLR" in task["scheduler"]
+        or "ReduceLROnPlateau" in task["scheduler"]
+        or "CosineAnnealingLR" in task["scheduler"]
+        or "ExponentialLR" in task["scheduler"]
+    ):
+        return json_config
+    op_type = task["scheduler"].get("type")
+    assert op_type is not None
+    if op_type == "step_lr":
+        op_config = {"StepLR": {}}
+        for key in ["step_size", "gamma"]:
+            if key in task["scheduler"]:
+                op_config["StepLR"][key] = task["scheduler"][key]
+    elif op_type == "lm_fine_tuning":
+        op_config = {"LmFineTuning": {}}
+        for key in [
+            "cut_frac",
+            "ratio",
+            "non_pretrained_param_groups",
+            "lm_lr_multiplier",
+            "lm_use_per_layer_lr",
+            "lm_gradual_unfreezing",
+            "last_epoch",
+        ]:
+            if key in task["scheduler"]:
+                op_config["LmFineTuning"][key] = task["scheduler"][key]
+    elif op_type == "reduce_lr_on_plateau":
+        op_config = {"ReduceLROnPlateau": {}}
+        for key in [
+            "lower_is_better",
+            "factor",
+            "patience",
+            "min_lr",
+            "threshold",
+            "threshold_is_absolute",
+            "cooldown",
+        ]:
+            if key in task["scheduler"]:
+                op_config["ReduceLROnPlateau"][key] = task["scheduler"][key]
+    elif op_type == "cosine_annealing_lr":
+        op_config = {"CosineAnnealingLR": {}}
+        for key in ["t_max", "eta_min"]:
+            if key in task["scheduler"]:
+                op_config["CosineAnnealingLR"][key] = task["scheduler"][key]
+    elif op_type == "exponential_lr":
+        op_config = {"ExponentialLR": {}}
+        for key in ["gamma"]:
+            if key in task["scheduler"]:
+                op_config["ExponentialLR"][key] = task["scheduler"][key]
+    else:
+        raise ValueError(
+            "Migration for your scheduler not supported. Please add it here."
+        )
+    task["scheduler"] = op_config
+    json_config["version"] = 2
+    return json_config
+
+
+adapters = {0: v0_to_v1, 1: v1_to_v2}
 
 
 def upgrade_one_version(json_config):
