4b7b48ba8bc3e088dbd1f9fa042c81e336800f00
Eric Gaudet <egaudet@fb.com>
2019-03-20 14:03:44 -0700

    plugin system to include user-defined components (#416)
    Summary:
    With this diff, PyText users can write tasks and components in their own directory and include them using --include <directory> without having to modify PyText sources to register their tasks, etc., which is very difficult when working with pip installed pytext-l=nlp. This works for all pytext CLI commands.
    
    Examples in upcoming tutorials.
    
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/416
    
    Reviewed By: snisarg
    
    Differential Revision: D14534038
    
    fbshipit-source-id: 898d15d284654685d2a1e92510fb5987c0293bc5

--- pytext/builtin_task.py
+++ pytext/builtin_task.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
+import glob
+import importlib
+import inspect
+import os
+
 from pytext.config.component import register_tasks
 from pytext.task.disjoint_multitask import DisjointMultitask
+from pytext.task.new_task import NewTask
+from pytext.task.task import Task
 from pytext.task.tasks import (
     ContextualIntentSlotTask,
     DocClassificationTask,
@@ -15,6 +22,37 @@ from pytext.task.tasks import (
     SeqNNTask,
     WordTaggingTask,
 )
+from pytext.utils.documentation import eprint
+
+
+USER_TASKS_DIR = "user_tasks"
+
+
+def add_include(path):
+    """
+    Import tasks (and associated components) from the folder name.
+    """
+    eprint("Including:", path)
+    modules = glob.glob(os.path.join(path, "*.py"))
+    all = [
+        os.path.basename(f)[:-3].replace("/", ".")
+        for f in modules
+        if os.path.isfile(f) and not f.endswith("__init__.py")
+    ]
+    for mod_name in all:
+        mod_path = path + "." + mod_name
+        eprint("... importing module:", mod_path)
+        my_module = importlib.import_module(mod_path)
+
+        for m in inspect.getmembers(my_module, inspect.isclass):
+            if m[1].__module__ != mod_path:
+                pass
+            elif Task in m[1].__bases__ or NewTask in m[1].__bases__:
+                eprint("... task:", m[1].__name__)
+                register_tasks(m[1])
+            else:
+                eprint("... importing:", m[1])
+                importlib.import_module(mod_path, m[1])
 
 
 def register_builtin_tasks():
