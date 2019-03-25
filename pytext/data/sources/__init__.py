3f51a9b4f06cade1dab02555ff4562e3ca8b62a5
Barlas Oguz <barlaso@fb.com>
2019-03-25 14:45:15 -0700

    SQuAD tensorizer (#383)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/383
    
    Tensorizer for training SQuAD on BERT.
    
    Reviewed By: kartikayk
    
    Differential Revision: D14328655
    
    fbshipit-source-id: 74c1f621ccdf1d5c9d972434d785e65477b9cead

--- pytext/data/sources/__init__.py
+++ pytext/data/sources/__init__.py
@@ -2,7 +2,8 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
 from .data_source import DataSource, RawExample
+from .squad import SquadDataSource
 from .tsv import TSVDataSource
 
 
-__all__ = ["DataSource", "RawExample", "TSVDataSource"]
+__all__ = ["DataSource", "RawExample", "SquadDataSource", "TSVDataSource"]
