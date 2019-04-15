eaf890d964c78a66ca0bce23ef14d68f547ab776
Eric Gaudet <egaudet@fb.com>
2019-04-15 15:50:22 -0700

    Release notes for v0.1.5

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(REQUIREMENTS) as f:
 
 setuptools.setup(
     name="pytext-nlp",
-    version="0.1.4",
+    version="0.1.5",
     description="pytorch modeling framework and model zoo for text models",
     url="https://github.com/facebookresearch/PyText",
     author="Facebook",
