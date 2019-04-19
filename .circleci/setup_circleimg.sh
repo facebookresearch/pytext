a615e97c29036d64c750e5e2f34bc0f46de49803
Ahmed Aly <ahhegazy@fb.com>
2019-04-19 11:25:27 -0700

    Fix circleci
    Summary: Pull Request resolved: https://github.com/facebookresearch/pytext/pull/482
    
    Reviewed By: seayoung1112
    
    Differential Revision: D14969544
    
    fbshipit-source-id: 702a256d52f7d381329349fe2cb677360ea2b5d7

--- .circleci/setup_circleimg.sh
+++ .circleci/setup_circleimg.sh
@@ -2,6 +2,5 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 sudo apt-get update
 sudo apt-get install -y cmake python-pip python-dev build-essential protobuf-compiler libprotoc-dev
-sudo pip install torch_nightly -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html
 sudo ./install_deps
 sudo pip install --progress-bar off pytest pytest-cov
