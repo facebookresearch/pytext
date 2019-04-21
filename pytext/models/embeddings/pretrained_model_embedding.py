cb1782b75f7c98f1411bdcb2818f3e2389ff2015
Kushal Lakhotia <kushall@fb.com>
2019-04-21 02:07:15 -0700

    Add support for exporting all PyText models that use contextual embeddings (#468)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/468
    
    1. Use ONNX Supported operators to inferring shape and reshaping tensors in PretrainedModelEmbedding.
    2. Input name for contextual embeddings are different for RNNG and JointBLSTM models. Fix this.
    
    Reviewed By: seayoung1112
    
    Differential Revision: D14863549
    
    fbshipit-source-id: 00a4ef852b30f27b266a6a5abc4fb5853611f0bc

--- pytext/models/embeddings/pretrained_model_embedding.py
+++ pytext/models/embeddings/pretrained_model_embedding.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-
 import torch
 from pytext.config.field_config import PretrainedModelEmbeddingConfig
 
@@ -17,11 +16,25 @@ class PretrainedModelEmbedding(EmbeddingBase):
         return cls(config.embed_dim)
 
     def forward(self, embedding: torch.Tensor) -> torch.Tensor:
-        if embedding.shape[1] % self.embedding_dim != 0:
+        embedding_shape = torch.onnx.operators.shape_as_tensor(embedding)
+
+        # Since embeddings vector is flattened, verify its shape correctness.
+        if embedding_shape[1].item() % self.embedding_dim != 0:
             raise ValueError(
-                f"Input embedding_dim {embedding.shape[1]} is not a"
+                f"Input embedding_dim {embedding_shape[1]} is not a"
                 + f" multiple of specified embedding_dim {self.embedding_dim}"
             )
-        num_tokens = embedding.shape[1] // self.embedding_dim
-        unflattened_embedding = embedding.view(-1, num_tokens, self.embedding_dim)
-        return unflattened_embedding
+
+        # Unflatten embedding Tensor from (batch_size, seq_len * embedding_size)
+        # to (batch_size, seq_len, embedding_size).
+        num_tokens = embedding_shape[1] // self.embedding_dim
+        new_embedding_shape = torch.cat(
+            (
+                torch.LongTensor([-1]),
+                num_tokens.view(1),
+                torch.LongTensor([self.embedding_dim]),
+            )
+        )
+        return torch.onnx.operators.reshape_from_tensor_shape(
+            embedding, new_embedding_shape
+        )
