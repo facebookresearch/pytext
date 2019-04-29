3d104c837648d4b3f3963fce13779ed93c9cff69
Eric Gaudet <egaudet@fb.com>
2019-04-29 14:30:32 -0700

    Adding missing copyrights
    Summary: Adding missing copyrights
    
    Differential Revision: D15135670
    
    fbshipit-source-id: 0223dc57fd50c8b28ab06811466e3f76c946c615

--- pytext/models/distributed.py
+++ pytext/models/distributed.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
 # This module is a copy of DistributedDataParallel before it was
 # switched to use the new reducer code in #18953.
 # We are debugging a problem where the loss becomes NaN after this
