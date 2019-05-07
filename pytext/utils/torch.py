01ee61765642af07db4c644659516b5358cf62ee
Arun Babu <arbabu@fb.com>
2019-05-07 11:13:56 -0700

    JIT Exporter for Seq2Seq (#542)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/542
    
    JIT Exporter for seq2seq model
    
    Facebook
    
    Note that to export the torchscript model, you need to add an extra parameter called `export_torchscript_path`
    Refer `fblearner/flow/projects/messenger/assistant/pytext/configs/test_rnn_seq2seq_torchscript.json` for example
    
    Reviewed By: seayoung1112
    
    Differential Revision: D14992339
    
    fbshipit-source-id: 3b96a3897c6671afca981c9bf854f3f84d0e3287

--- pytext/utils/torch.py
+++ pytext/utils/torch.py
@@ -3,7 +3,7 @@
 
 from typing import Dict, List
 
-from torch import jit
+from torch import Tensor, jit
 
 
 @jit.script
@@ -14,11 +14,30 @@ def list_max(l: List[int]):
     return max_value
 
 
+@jit.script
+def list_membership(item: int, list: List[int]):
+    item_present = False
+    for i in list:
+        if item == i:
+            item_present = True
+    return item_present
+
+
 class Vocabulary(jit.ScriptModule):
-    def __init__(self, vocab_list, unk_idx: int = 0):
+    def __init__(
+        self,
+        vocab_list,
+        unk_idx: int = 0,
+        pad_idx: int = -1,
+        bos_idx: int = -1,
+        eos_idx: int = -1,
+    ):
         super().__init__()
         self.vocab = jit.Attribute(vocab_list, List[str])
         self.unk_idx = jit.Attribute(unk_idx, int)
+        self.pad_idx = jit.Attribute(pad_idx, int)
+        self.eos_idx = jit.Attribute(eos_idx, int)
+        self.bos_idx = jit.Attribute(bos_idx, int)
         self.idx = jit.Attribute(
             {word: i for i, word in enumerate(vocab_list)}, Dict[str, int]
         )
@@ -35,4 +54,18 @@ class Vocabulary(jit.ScriptModule):
         result = jit.annotate(List[List[int]], [])
         for value in values:
             result.append(self.lookup_indices_1d(value))
+        return result
+
+    @jit.script_method
+    def lookup_words_1d(
+        self, values: Tensor, filter_token_list: List[int] = ()
+    ) -> List[str]:
+        result = jit.annotate(List[str], [])
+        for idx in range(values.size(0)):
+            value = int(values[idx])
+            if not list_membership(value, filter_token_list):
+                if value < len(self.vocab):
+                    result.append(self.vocab[int(value)])
+                else:
+                    result.append(self.vocab[self.unk_idx])
         return result
