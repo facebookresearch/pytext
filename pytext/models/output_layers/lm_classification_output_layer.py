9fad33e158560648a744078cb2f632327f56dfbd
Barlas Oguz <barlaso@fb.com>
2019-05-14 16:46:36 -0700

    packed lm w/ next sentence prediction
    Summary:
    Implement next sentence prediction jointly with masked lm training for BERT.
    
    Design:
    Data class assumes 2 independent streams of text.  When packing a batch, for each row, roughly half the row will be packed from one stream, the other half will either be packed from the same stream (positive sample) or switch and pack from the alternate stream (negative sample).  The break point will respect sentence boundary.  We try to have the break point fall inside the 0.25-0.75 interval of the row whenever possible.  Document boundaries are not handled (i.e. sentences from conrtiguous documents will be regarded as positive sentence).
    
    The 2 text streams are created by the negative sampling data source from a single text file.  Two file handles are created, one of which seeks to the middle of the file.  Each row has two sentences, one from each stream.
    
    The model is an extension of MaskedLM, just adds an mlp on the pooled output of encoder to predict next sentence.
    
    Differential Revision: D14728414
    
    fbshipit-source-id: c4fbc1eb3c4684ad8d23fade2df3a1f45b7773cd

---
+++ pytext/models/output_layers/lm_classification_output_layer.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+import math
+from typing import Any, Dict, List, Tuple
+
+import torch
+from pytext.config.component import create_loss
+from pytext.data.utils import PAD
+from pytext.loss import CrossEntropyLoss, Loss
+
+from .doc_classification_output_layer import ClassificationOutputLayer
+from .lm_output_layer import LMOutputLayer
+from .output_layer_base import OutputLayerBase
+
+
+class LMClassificationOutputLayer(LMOutputLayer):
+    class Config(OutputLayerBase.Config):
+        loss: CrossEntropyLoss.Config = CrossEntropyLoss.Config()
+
+    @classmethod
+    def from_config(cls, config: Config, vocab=None, labels=None):
+        pad_token_idx = vocab.idx[PAD]
+        return cls(
+            list(vocab),
+            list(labels),
+            create_loss(config.loss, ignore_index=pad_token_idx),
+            create_loss(config.loss),
+            pad_token_idx=pad_token_idx,
+        )
+
+    def __init__(
+        self,
+        vocab: List[str],
+        labels: List[str],
+        lm_loss_fn: Loss = None,
+        classification_loss_fn: Loss = None,
+        config=None,
+        pad_token_idx=-100,
+    ):
+        super().__init__(vocab, lm_loss_fn, config, pad_token_idx)
+        self.classification_output = ClassificationOutputLayer(
+            labels, classification_loss_fn, config
+        )
+
+    def get_loss(
+        self,
+        logit: torch.Tensor,
+        target: torch.Tensor,
+        context: Dict[str, Any],
+        reduce=True,
+    ) -> torch.Tensor:
+        lm_loss = super().get_loss(logit[0], target, context, reduce)
+        classification_loss = self.classification_output.get_loss(
+            logit[1], target[-1], context, reduce
+        )
+        return lm_loss + classification_loss, lm_loss, classification_loss
+
+    def get_pred(
+        self, logit: torch.Tensor, *args, **kwargs
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        return super().get_pred(logit[0], *args, **kwargs)
