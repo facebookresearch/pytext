cbff3621e76745cbc4a8b7b8b795125ce6144b1c
Barlas Oguz <barlaso@fb.com>
2019-05-14 16:46:44 -0700

    packed lm w/ next sentence prediction
    Summary:
    Implement next sentence prediction jointly with masked lm training for BERT.
    
    Design:
    Data class assumes 2 independent streams of text.  When packing a batch, for each row, roughly half the row will be packed from one stream, the other half will either be packed from the same stream (positive sample) or switch and pack from the alternate stream (negative sample).  The break point will respect sentence boundary.  We try to have the break point fall inside the 0.25-0.75 interval of the row whenever possible.  Document boundaries are not handled (i.e. sentences from conrtiguous documents will be regarded as positive sentence).
    
    The 2 text streams are created by the negative sampling data source from a single text file.  Two file handles are created, one of which seeks to the middle of the file.  Each row has two sentences, one from each stream.
    
    The model is an extension of MaskedLM, just adds an mlp on the pooled output of encoder to predict next sentence.
    
    Differential Revision: D14728414
    
    fbshipit-source-id: 9022b16fe100f14e0d020f1045b3137a23095a5f

--- pytext/data/sources/__init__.py
+++ pytext/data/sources/__init__.py
@@ -2,8 +2,15 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
 from .data_source import DataSource, RawExample
+from .negative_sampling_data_source import NegativeSamplingDataSource
 from .squad import SquadDataSource
 from .tsv import TSVDataSource
 
 
-__all__ = ["DataSource", "RawExample", "SquadDataSource", "TSVDataSource"]
+__all__ = [
+    "DataSource",
+    "NegativeSamplingDataSource",
+    "RawExample",
+    "SquadDataSource",
+    "TSVDataSource",
+]
