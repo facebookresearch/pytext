64ab1835905dea2e7797e6bc11398c55941fa728
Eric Gaudet <egaudet@fb.com>
2019-05-20 14:36:39 -0700

    port LMTask (#580)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/580
    
    This diff is migrating LMTask to the new data handler.
    
    I'm publishing it now to start the review process. It's still missing
    support for BPTTLanguageModelDataHandler.
    
    Reviewed By: m3rlin45
    
    Differential Revision: D15306415
    
    fbshipit-source-id: ac7d285cc940c726f45f1d8e0238350b30e521d2

--- pytext/models/language_models/lmlstm.py
+++ pytext/models/language_models/lmlstm.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
-from typing import List, Optional, Tuple, Union
+from typing import Dict, List, Optional, Tuple, Union
 
 import torch
 from pytext.config import ConfigBase
 from pytext.data import CommonMetadata
+from pytext.data.tensorizers import RawString, Tensorizer, TokenTensorizer
+from pytext.models.decoders import DecoderBase
 from pytext.models.decoders.mlp_decoder import MLPDecoder
-from pytext.models.model import Model
+from pytext.models.embeddings import EmbeddingBase
+from pytext.models.embeddings.word_embedding import WordEmbedding
+from pytext.models.model import BaseModel, Model
+from pytext.models.module import create_module
+from pytext.models.output_layers import OutputLayerBase
 from pytext.models.output_layers.lm_output_layer import LMOutputLayer
 from pytext.models.representations.bilstm import BiLSTM
+from pytext.models.representations.representation_base import RepresentationBase
 
 
 def repackage_hidden(
@@ -32,11 +39,12 @@ def repackage_hidden(
         return tuple(repackage_hidden(v) for v in hidden)
 
 
-class LMLSTM(Model):
+class LMLSTM_Deprecated(Model):
     """
     `LMLSTM` implements a word-level language model that uses LSTMs to
     represent the document.
 
+    DEPRECATED: Use LMLSTM instead
     """
 
     class Config(ConfigBase):
@@ -136,6 +144,127 @@ class LMLSTM(Model):
         # TODO make hidden states initialization more generalized
         weight = next(self.parameters())
         num_layers = self.representation.num_layers
+        rnn_hidden_dim = self.representation.representation_dim
+        return (
+            weight.new_zeros(bsz, num_layers, rnn_hidden_dim),
+            weight.new_zeros(bsz, num_layers, rnn_hidden_dim),
+        )
+
+
+class LMLSTM(BaseModel):
+    """
+    `LMLSTM` implements a word-level language model that uses LSTMs to
+    represent the document.
+
+    """
+
+    class Config(BaseModel.Config):
+        class ModelInput(Model.Config.ModelInput):
+            tokens: TokenTensorizer.Config = TokenTensorizer.Config(
+                add_bos_token=True, add_eos_token=True
+            )
+            # for metric reporter
+            raw_text: RawString.Config = RawString.Config(column="text")
+
+        inputs: ModelInput = ModelInput()
+        embedding: WordEmbedding.Config = WordEmbedding.Config()
+        representation: BiLSTM.Config = BiLSTM.Config(bidirectional=False)
+        decoder: Optional[MLPDecoder.Config] = MLPDecoder.Config()
+        output_layer: LMOutputLayer.Config = LMOutputLayer.Config()
+        tied_weights: bool = False
+        stateful: bool = False
+
+    @classmethod
+    def from_config(cls, config: Config, tensorizers: Dict[str, Tensorizer]):
+        embedding = create_module(config.embedding, tensorizer=tensorizers["tokens"])
+        representation = create_module(
+            config.representation, embed_dim=embedding.embedding_dim
+        )
+        labels = tensorizers["tokens"].vocab
+        decoder = create_module(
+            config.decoder,
+            in_dim=representation.representation_dim,
+            out_dim=len(labels),
+        )
+        if config.tied_weights:
+            decoder.get_decoder()[0].weight = embedding.weight
+        output_layer = create_module(config.output_layer, labels=labels)
+        return cls(
+            embedding=embedding,
+            representation=representation,
+            decoder=decoder,
+            output_layer=output_layer,
+            stateful=config.stateful,
+        )
+
+    def __init__(
+        self,
+        embedding: EmbeddingBase = Config.embedding,
+        representation: RepresentationBase = Config.representation,
+        decoder: DecoderBase = Config.decoder,
+        output_layer: OutputLayerBase = Config.output_layer,
+        stateful: bool = Config.stateful,
+    ) -> None:
+        super().__init__()
+        self.embedding = embedding
+        self.representation = representation
+        self.decoder = decoder
+        self.output_layer = output_layer
+        self.stateful = stateful
+        self.module_list = [embedding, representation, decoder]
+        self._states: Optional[Tuple] = None
+
+    def arrange_model_inputs(self, tensor_dict):
+        tokens, seq_lens = tensor_dict["tokens"]
+        # Omit last token because it won't have a corresponding target
+        return (tokens[:, 0:-1].contiguous(), seq_lens - 1)
+
+    def arrange_targets(self, tensor_dict):
+        # Omit first token because it won't have a corresponding input
+        return tensor_dict["tokens"][0][:, 1:].contiguous()
+
+    def get_export_input_names(self, tensorizers):
+        return ["tokens", "tokens_lens"]
+
+    def get_export_output_names(self, tensorizers):
+        return ["scores"]
+
+    def vocab_to_export(self, tensorizers):
+        return {"tokens": list(tensorizers["tokens"].vocab)}
+
+    def forward(
+        self, tokens: torch.Tensor, seq_len: torch.Tensor
+    ) -> List[torch.Tensor]:
+        token_emb = self.embedding(tokens)
+        if self.stateful and self._states is None:
+            self._states = self.init_hidden(tokens.size(0))
+
+        rep, states = self.representation(token_emb, seq_len, states=self._states)
+        if self.decoder is None:
+            output = rep
+        else:
+            if not isinstance(rep, (list, tuple)):
+                rep = [rep]
+            output = self.decoder(*rep)
+
+        if self.stateful:
+            self._states = repackage_hidden(states)
+        return output  # (bsz, nclasses)
+
+    def init_hidden(self, bsz: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Initialize the hidden states of the LSTM if the language model is
+        stateful.
+
+        Args:
+            bsz (int): Batch size.
+
+        Returns:
+            Tuple[torch.Tensor, torch.Tensor]: Initialized hidden state and
+            cell state of the LSTM.
+        """
+        weight = next(self.parameters())
+        num_layers = self.representation.lstm.num_layers
         rnn_hidden_dim = self.representation.representation_dim
         return (
             weight.new_zeros(bsz, num_layers, rnn_hidden_dim),
