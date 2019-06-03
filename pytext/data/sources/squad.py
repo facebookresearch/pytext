55d45cd3aca04581a3f28e065d6527fb9c36b421
Kushal Lakhotia <kushall@fb.com>
2019-06-03 13:24:11 -0700

    Add TSV support for Squad
    Differential Revision: D15590482
    
    fbshipit-source-id: 565cfb9ceada2381c5fe009c232a3b40d691d437

--- pytext/data/sources/squad.py
+++ pytext/data/sources/squad.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
 import json
 from typing import List, Optional
 
 from pytext.data.sources.data_source import DataSource, generator_property
+from pytext.data.sources.tsv import TSV, SafeFileWrapper
 
 
-def unflatten(fname, ignore_impossible):
+def get_json_iter(fname, ignore_impossible):
     if not fname:
         return
     with open(fname) as file:
@@ -32,6 +34,23 @@ def unflatten(fname, ignore_impossible):
                     }
 
 
+def get_tsv_iter(file_path, field_names, delimiter):
+    for row in iter(
+        TSV(
+            SafeFileWrapper(file_path, encoding="utf-8", errors="replace"),
+            field_names=field_names,
+            delimiter=delimiter,
+        )
+    ):
+        yield {
+            "doc": row["doc"],
+            "question": row["question"],
+            "answers": json.loads(row["answers"]),
+            "answer_starts": json.loads(row["answer_starts"]),
+            "has_answer": json.loads(row["has_answer"]),
+        }
+
+
 class SquadDataSource(DataSource):
     """
     Download data from https://rajpurkar.github.io/SQuAD-explorer/
@@ -43,6 +62,15 @@ class SquadDataSource(DataSource):
         test_filename: Optional[str] = "dev-v2.0.json"
         eval_filename: Optional[str] = "dev-v2.0.json"
         ignore_impossible: bool = True
+        is_json: bool = True
+        field_names: List[str] = [
+            "doc",
+            "question",
+            "answers",
+            "answer_starts",
+            "has_answer",
+        ]
+        delimiter: str = "\t"
 
     @classmethod
     def from_config(cls, config: Config, schema=None):
@@ -51,6 +79,9 @@ class SquadDataSource(DataSource):
             config.test_filename,
             config.eval_filename,
             config.ignore_impossible,
+            config.is_json,
+            config.field_names,
+            config.delimiter,
         )
 
     def __init__(
@@ -59,6 +90,9 @@ class SquadDataSource(DataSource):
         test_filename=None,
         eval_filename=None,
         ignore_impossible=Config.ignore_impossible,
+        is_json=Config.is_json,
+        field_names=Config.field_names,
+        delimiter=Config.delimiter,
     ):
         schema = {
             "doc": str,
@@ -73,15 +107,30 @@ class SquadDataSource(DataSource):
         self.test_filename = test_filename
         self.eval_filename = eval_filename
         self.ignore_impossible = ignore_impossible
+        self.is_json = is_json
+        self.field_names = field_names
+        self.delimiter = delimiter
 
     @generator_property
     def train(self):
-        return unflatten(self.train_filename, self.ignore_impossible)
+        return (
+            get_json_iter(self.train_filename, self.ignore_impossible)
+            if self.is_json
+            else get_tsv_iter(self.train_filename, self.field_names, self.delimiter)
+        )
 
     @generator_property
     def test(self):
-        return unflatten(self.test_filename, self.ignore_impossible)
+        return (
+            get_json_iter(self.test_filename, self.ignore_impossible)
+            if self.is_json
+            else get_tsv_iter(self.train_filename, self.field_names, self.delimiter)
+        )
 
     @generator_property
     def eval(self):
-        return unflatten(self.eval_filename, self.ignore_impossible)
+        return (
+            get_json_iter(self.eval_filename, self.ignore_impossible)
+            if self.is_json
+            else get_tsv_iter(self.train_filename, self.field_names, self.delimiter)
+        )
