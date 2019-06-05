91f2de9d76926f2cade73fd5d6262177a3b8f859
Kushal Lakhotia <kushall@fb.com>
2019-06-05 11:55:59 -0700

    Fix setting random seed when running on CuDNN backend (#663)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/663
    
    Set CuDNN's behavior to be deterministic when training on GPU and when PyText's `use_deterministic_cudnn` flag is set.
    
    Reviewed By: borguz
    
    Differential Revision: D15628927
    
    fbshipit-source-id: 7f64fc9899d02ccbbe4caf14be9f70a4e1180f50

--- pytext/utils/__init__.py
+++ pytext/utils/__init__.py
@@ -6,11 +6,23 @@ def cls_vars(cls):
     return [v for n, v in vars(cls).items() if not n.startswith("_")]
 
 
-def set_random_seeds(seed):
+def set_random_seeds(seed, use_deterministic_cudnn):
     import random
     import numpy as np
     import torch
+    from pytext.utils import cuda
 
+    # See https://pytorch.org/docs/stable/notes/randomness.html
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
+    if cuda.CUDA_ENABLED and use_deterministic_cudnn:
+        print(
+            """WARNING: Your training might be slower because you have set
+        use_deterministic_cudnn flag to True. Read
+        https://pytorch.org/docs/stable/notes/randomness.html and
+        https://discuss.pytorch.org/t/what-is-the-differenc-between-cudnn-deterministic-and-cudnn-benchmark/38054
+        """
+        )
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
