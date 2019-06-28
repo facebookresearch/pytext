161294746cfe445a7cd92dbfd28ddecee706a9e6
Barlas Oguz <barlaso@fb.com>
2019-06-28 14:36:46 -0700

    open source MaskedLM and BERT models (#734)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/734
    
    Open source BERT classification, regression models,  MaskedLM model and assosciated tasks.
    
    Reviewed By: kartikayk
    
    Differential Revision: D16022039
    
    fbshipit-source-id: 673953c4323355bdf060bfd944dd081bcc61a3f3

---
+++ pytext/models/bert_classification_models.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+from typing import Dict, List, Optional, Tuple
+
+import torch
+import torch.nn as nn
+from pytext.common.constants import Stage
+from pytext.config.component import create_loss
+from pytext.data.bert_tensorizer import BERTTensorizer
+from pytext.data.tensorizers import (
+    FloatListTensorizer,
+    LabelTensorizer,
+    NtokensTensorizer,
+    Tensorizer,
+)
+from pytext.loss import BinaryCrossEntropyLoss, MultiLabelSoftMarginLoss
+from pytext.models.decoders.mlp_decoder import MLPDecoder
+from pytext.models.model import BaseModel, ModelInputBase
+from pytext.models.module import create_module
+from pytext.models.output_layers import ClassificationOutputLayer
+from pytext.models.output_layers.doc_classification_output_layer import (
+    BinaryClassificationOutputLayer,
+    MulticlassOutputLayer,
+    MultiLabelOutputLayer,
+)
+from pytext.models.pair_classification_model import BasePairwiseModel
+from pytext.models.representations.huggingface_bert_sentence_encoder import (
+    HuggingFaceBertSentenceEncoder,
+)
+from pytext.models.representations.transformer_sentence_encoder_base import (
+    TransformerSentenceEncoderBase,
+)
+
+
+class NewBertModel(BaseModel):
+    """BERT single sentence classification."""
+
+    SUPPORT_FP16_OPTIMIZER = True
+
+    class Config(BaseModel.Config):
+        class BertModelInput(BaseModel.Config.ModelInput):
+            tokens: BERTTensorizer.Config = BERTTensorizer.Config(max_seq_len=128)
+            dense: Optional[FloatListTensorizer.Config] = None
+            labels: LabelTensorizer.Config = LabelTensorizer.Config()
+            # for metric reporter
+            num_tokens: NtokensTensorizer.Config = NtokensTensorizer.Config(
+                names=["tokens"], indexes=[2]
+            )
+
+        inputs: BertModelInput = BertModelInput()
+        encoder: TransformerSentenceEncoderBase.Config = (
+            HuggingFaceBertSentenceEncoder.Config()
+        )
+        decoder: MLPDecoder.Config = MLPDecoder.Config()
+        output_layer: ClassificationOutputLayer.Config = (
+            ClassificationOutputLayer.Config()
+        )
+
+    def arrange_model_inputs(self, tensor_dict):
+        model_inputs = (tensor_dict["tokens"],)
+        if "dense" in tensor_dict:
+            model_inputs += (tensor_dict["dense"],)
+        return model_inputs
+
+    def arrange_targets(self, tensor_dict):
+        return tensor_dict["labels"]
+
+    def forward(
+        self, encoder_inputs: Tuple[torch.Tensor, ...], *args
+    ) -> List[torch.Tensor]:
+        representation = self.encoder(encoder_inputs)[0]
+        return self.decoder(representation, *args)
+
+    def caffe2_export(self, tensorizers, tensor_dict, path, export_onnx_path=None):
+        pass
+
+    @classmethod
+    def from_config(cls, config: Config, tensorizers: Dict[str, Tensorizer]):
+        labels = tensorizers["labels"].vocab
+        vocab = tensorizers["tokens"].vocab
+        encoder = create_module(
+            config.encoder,
+            padding_idx=vocab.get_pad_index(),
+            vocab_size=vocab.__len__(),
+        )
+        dense_dim = tensorizers["dense"].dim if "dense" in tensorizers else 0
+        decoder = create_module(
+            config.decoder,
+            in_dim=encoder.representation_dim + dense_dim,
+            out_dim=len(labels),
+        )
+
+        loss = create_loss(config.output_layer.loss)
+
+        if isinstance(loss, BinaryCrossEntropyLoss):
+            output_layer_cls = BinaryClassificationOutputLayer
+        elif isinstance(loss, MultiLabelSoftMarginLoss):
+            output_layer_cls = MultiLabelOutputLayer
+        else:
+            output_layer_cls = MulticlassOutputLayer
+
+        output_layer = output_layer_cls(list(labels), loss)
+        return cls(encoder, decoder, output_layer)
+
+    def __init__(self, encoder, decoder, output_layer, stage=Stage.TRAIN) -> None:
+        super().__init__(stage=stage)
+        self.encoder = encoder
+        self.decoder = decoder
+        self.module_list = [encoder, decoder]
+        self.output_layer = output_layer
+        self.stage = stage
+        self.module_list = [encoder, decoder]
+
+
+class BertPairwiseModel(BasePairwiseModel):
+    """Bert Pairwise classification model
+
+    The model takes two sets of tokens (left and right), calculates their
+    representations separately using shared BERT encoder and passes them to
+    the decoder along with their absolute difference and elementwise product,
+    all concatenated. Used for e.g. natural language inference.
+    """
+
+    class Config(BasePairwiseModel.Config):
+        class ModelInput(ModelInputBase):
+            tokens1: BERTTensorizer.Config = BERTTensorizer.Config(
+                columns=["text1"], max_seq_len=128
+            )
+            tokens2: BERTTensorizer.Config = BERTTensorizer.Config(
+                columns=["text2"], max_seq_len=128
+            )
+            labels: LabelTensorizer.Config = LabelTensorizer.Config()
+            # for metric reporter
+            num_tokens: NtokensTensorizer.Config = NtokensTensorizer.Config(
+                names=["tokens1", "tokens2"], indexes=[2, 2]
+            )
+
+        inputs: ModelInput = ModelInput()
+        encoder: TransformerSentenceEncoderBase.Config = (
+            HuggingFaceBertSentenceEncoder.Config()
+        )
+        shared_encoder: bool = True
+
+    def __init__(
+        self, encoder1, encoder2, decoder, output_layer, encode_relations
+    ) -> None:
+        super().__init__(decoder, output_layer, encode_relations)
+        self.encoder1 = encoder1
+        self.encoder2 = encoder2
+
+    @classmethod
+    def _create_encoder(
+        cls, config: Config, tensorizers: Dict[str, Tensorizer]
+    ) -> nn.ModuleList:
+        encoder1 = create_module(
+            config.encoder,
+            padding_idx=tensorizers["tokens1"].vocab.get_pad_index(),
+            vocab_size=len(tensorizers["tokens1"].vocab),
+        )
+        if config.shared_encoder:
+            encoder2 = encoder1
+        else:
+            encoder2 = create_module(
+                config.encoder,
+                padding_idx=tensorizers["tokens2"].vocab.get_pad_index(),
+                vocab_size=len(tensorizers["tokens2"].vocab),
+            )
+        return encoder1, encoder2
+
+    @classmethod
+    def from_config(cls, config: Config, tensorizers: Dict[str, Tensorizer]):
+        encoder1, encoder2 = cls._create_encoder(config, tensorizers)
+        decoder, output_layer = cls._create_decoder(
+            config, [encoder1, encoder2], tensorizers
+        )
+        return cls(encoder1, encoder2, decoder, output_layer, config.encode_relations)
+
+    def arrange_model_inputs(self, tensor_dict):
+        return tensor_dict["tokens1"], tensor_dict["tokens2"]
+
+    def arrange_targets(self, tensor_dict):
+        return tensor_dict["labels"]
+
+    def forward(
+        self,
+        input_tuple1: Tuple[torch.Tensor, ...],
+        input_tuple2: Tuple[torch.Tensor, ...],
+    ) -> torch.Tensor:
+        encodings = [self.encoder1(input_tuple1)[0], self.encoder2(input_tuple2)[0]]
+        if self.encode_relations:
+            encodings = self._encode_relations(encodings)
+        encoding = torch.cat(encodings, -1)
+        return self.decoder(encoding)
+
+    def save_modules(self, base_path: str = "", suffix: str = ""):
+        self._save_modules(self.encoders, base_path, suffix)
---
+++ pytext/models/masked_lm.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+from typing import Dict, List
+
+import torch
+from fairseq.modules.transformer_sentence_encoder import init_bert_params
+from pytext.common.constants import Stage
+from pytext.config import ConfigBase
+from pytext.data.bert_tensorizer import BERTTensorizer
+from pytext.data.tensorizers import Tensorizer
+from pytext.data.utils import BOS, EOS, MASK, Vocabulary
+from pytext.models.decoders.mlp_decoder import MLPDecoder
+from pytext.models.masking_utils import (
+    MaskingStrategy,
+    frequency_based_masking,
+    random_masking,
+)
+from pytext.models.model import BaseModel
+from pytext.models.module import create_module
+from pytext.models.output_layers.lm_output_layer import LMOutputLayer
+from pytext.models.representations.transformer_sentence_encoder import (
+    TransformerSentenceEncoder,
+)
+from pytext.models.representations.transformer_sentence_encoder_base import (
+    TransformerSentenceEncoderBase,
+)
+
+
+class MaskedLanguageModel(BaseModel):
+    """Masked language model for BERT style pre-training."""
+
+    SUPPORT_FP16_OPTIMIZER = True
+
+    class Config(BaseModel.Config):
+        class InputConfig(ConfigBase):
+            tokens: BERTTensorizer.Config = BERTTensorizer.Config(max_seq_len=128)
+
+        inputs: InputConfig = InputConfig()
+        encoder: TransformerSentenceEncoderBase.Config = TransformerSentenceEncoder.Config()
+        decoder: MLPDecoder.Config = MLPDecoder.Config()
+        output_layer: LMOutputLayer.Config = LMOutputLayer.Config()
+        mask_prob: float = 0.15
+        mask_bos: bool = False
+        # masking
+        masking_strategy: MaskingStrategy = MaskingStrategy.RANDOM
+        # tie weights determines whether the input embedding weights are used
+        # in the output vocabulary projection as well
+        tie_weights: bool = True
+
+    @classmethod
+    def from_config(cls, config: Config, tensorizers: Dict[str, Tensorizer]):
+        token_tensorizer = tensorizers["tokens"]
+        vocab = token_tensorizer.vocab
+
+        encoder = create_module(
+            config.encoder,
+            output_encoded_layers=True,
+            padding_idx=vocab.get_pad_index(),
+            vocab_size=vocab.__len__(),
+        )
+        decoder = create_module(
+            config.decoder, in_dim=encoder.representation_dim, out_dim=len(vocab)
+        )
+        if getattr(config.encoder, "apply_bert_init", False):
+            decoder.apply(init_bert_params)
+        # if weights are not shared then we need to ensure that the decoder
+        # params are initialized in the same was as the encoder params
+        if config.tie_weights:
+            list(decoder.mlp.modules())[-1].weight = encoder._embedding().weight
+
+        output_layer = create_module(config.output_layer, labels=vocab)
+        return cls(
+            encoder,
+            decoder,
+            output_layer,
+            token_tensorizer,
+            vocab,
+            mask_prob=config.mask_prob,
+            mask_bos=config.mask_bos,
+            masking_strategy=config.masking_strategy,
+        )
+
+    def __init__(
+        self,
+        encoder: TransformerSentenceEncoderBase,
+        decoder: MLPDecoder,
+        output_layer: LMOutputLayer,
+        token_tensorizer: BERTTensorizer,
+        vocab: Vocabulary,
+        mask_prob: float = Config.mask_prob,
+        mask_bos: float = Config.mask_bos,
+        masking_strategy: MaskingStrategy = Config.masking_strategy,
+        stage: Stage = Stage.TRAIN,
+    ) -> None:
+        super().__init__(stage=stage)
+        self.encoder = encoder
+        self.decoder = decoder
+        self.module_list = [encoder, decoder]
+        self.output_layer = output_layer
+        self.token_tensorizer = token_tensorizer
+        self.vocab = vocab
+        self.mask_prob = mask_prob
+        self.mask_bos = mask_bos
+        self.stage = stage
+        self.masking_strategy = masking_strategy
+
+        # initialize the frequency based sampling weights if these will be used
+        self.token_sampling_weights = None
+        if self.masking_strategy == MaskingStrategy.FREQUENCY:
+            self.token_sampling_weights = [x ** -0.5 for x in self.vocab.counts]
+
+    def arrange_model_inputs(self, tensor_dict):
+        tokens, *other = tensor_dict["tokens"]
+        self.mask, self.pad_mask, mask_mask, rand_mask = self._get_mask(tokens)
+        masked_tokens = self._mask_input(tokens, mask_mask, self.vocab.idx[MASK])
+        masked_tokens = self._mask_input(
+            masked_tokens, rand_mask, torch.randint_like(tokens, high=len(self.vocab))
+        )
+        return (masked_tokens,) + tuple(other)
+
+    def arrange_targets(self, tensor_dict):
+        tokens, *other = tensor_dict["tokens"]
+        masked_target = self._mask_output(tokens, self.mask)
+        # (masked targets, #predicted tokens, #input tokens)
+        return masked_target, self.mask.sum(-1), self.pad_mask.sum(-1)
+
+    def forward(self, *inputs) -> List[torch.Tensor]:
+        encoded_layers, _ = self.encoder(inputs)
+        return self.decoder(encoded_layers[-1])
+
+    def _select_tokens_to_mask(
+        self, tokens: torch.Tensor, mask_prob: float
+    ) -> torch.tensor:
+        if self.masking_strategy == MaskingStrategy.RANDOM:
+            return random_masking(tokens, mask_prob)
+        elif self.masking_strategy == MaskingStrategy.FREQUENCY:
+            return frequency_based_masking(
+                tokens, self.token_sampling_weights, mask_prob
+            )
+        else:
+            raise NotImplementedError(
+                "Specified Masking Strategy isnt currently implemented."
+            )
+
+    def _get_mask(self, tokens):
+        mask = self._select_tokens_to_mask(tokens, self.mask_prob)
+        pad_mask = (tokens != self.vocab.get_pad_index()).long()
+        mask *= pad_mask
+
+        if not self.mask_bos:
+            bos_idx = (
+                self.vocab.idx[EOS]
+                if self.token_tensorizer.use_eos_token_for_bos
+                else self.vocab.idx[BOS]
+            )
+            mask *= (tokens != bos_idx).long()
+
+        probs = torch.rand_like(tokens, dtype=torch.float)
+        rand_mask = (probs < 0.1).long() * mask
+        mask_mask = (probs >= 0.2).long() * mask
+        return mask, pad_mask, mask_mask, rand_mask
+
+    def _mask_input(self, tokens, mask, replacement):
+        return tokens * (1 - mask) + replacement * mask
+
+    def _mask_output(self, tokens, mask):
+        return tokens * mask + self.vocab.get_pad_index() * (1 - mask)
