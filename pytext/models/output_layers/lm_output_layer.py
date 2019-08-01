fixed overflow error in lm reporting (#831)
Summary:
Pull Request resolved: https://github.com/facebookresearch/pytext/pull/831

Fixes overflow error in perplexity reporting by showing "inf" instead of OverflowError

Reviewed By: arbabu123

Differential Revision: D16495447

fbshipit-source-id: 20f64910c07386c22f3ba6e728b40764f3d8f15a

pytext/models/output_layers/lm_output_layer.py
@@ -125,4 +125,8 @@ class LMOutputLayer(OutputLayerBase):
 
     @staticmethod
     def calculate_perplexity(sequence_loss: torch.Tensor) -> torch.Tensor:
-        return torch.exp(sequence_loss)
+        try:
+            ppl = torch.exp(sequence_loss)
+        except OverflowError:
+            ppl = float("inf")
+        return ppl