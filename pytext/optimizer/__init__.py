88d408fb973648e57f03e0dd767e577df20f578e
Armen Aghajanyan <armenag@fb.com>
2019-09-05 12:01:38 -0700

    Add AdamW
    Summary: Add support for new AdamW algorithm.
    
    Differential Revision: D17135432
    
    fbshipit-source-id: 5433bbe23ab00a8fc9209001bb83f0d662c40154

--- pytext/optimizer/__init__.py
+++ pytext/optimizer/__init__.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from pytext.optimizer.activations import get_activation
-from pytext.optimizer.optimizers import SGD, Adagrad, Adam, Optimizer, learning_rates
+from pytext.optimizer.optimizers import (
+    SGD,
+    Adagrad,
+    Adam,
+    AdamW,
+    Optimizer,
+    learning_rates,
+)
 from pytext.optimizer.swa import StochasticWeightAveraging
