ba21aff4c3087056b2fd3e6ade0e8002506d7c91
Steven Liu <stevenliu@fb.com>
2019-10-09 15:15:25 -0700

    Silent PYTEXT_HOME print/exception during importing (#1036)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/1036
    
    1. PYTEXT_HOME will add unrelated print in some cases, e.g. PyText -> Fluent2 -> other non-PyText project using Fluent2)
    2. Corner case: PyText is imported as a module and packed into a single binary file,  `__file__` will be the path of the binary file instead of path.py and cause exception
    3. In this case, we set PYTEXT_HOME to be the folder of that binary file and assuming the data files will be put either in the same folder or providing a absolute path in config file
    
    Reviewed By: chenyangyu1988
    
    Differential Revision: D17796032
    
    fbshipit-source-id: a367b3d662c6700ad48008010ed6c0713b24a3c6

--- pytext/utils/path.py
+++ pytext/utils/path.py
@@ -7,23 +7,27 @@ def get_pytext_home():
     internal_home = os.path.realpath(os.path.join(__file__, "../../"))
     oss_home = os.path.realpath(os.path.join(__file__, "../../../"))
     default_home = ""
+    # use tests as anchor which will always in PYTEXT_HOME/tests
     if os.path.exists(os.path.join(internal_home, "tests")):
         default_home = internal_home
     elif os.path.exists(os.path.join(oss_home, "tests")):
         default_home = oss_home
     else:
-        raise Exception("Can't find PYTEXT_HOME")
+        # when PyText is used as a module and packed as part of a single file X
+        # __file__ will be path of X instead of path.py
+        # in these case, PYTEXT_HOME will be the parent folder of X
+        default_home = os.path.dirname(__file__)
     pytext_home = os.environ.get("PYTEXT_HOME", default_home)
-    print(f"PYTEXT_HOME: {pytext_home}")
     return pytext_home
 
 
+# all relateive path in PyText is based on PYTEXT_HOME
 PYTEXT_HOME = get_pytext_home()
 
 
-def get_absolute_path(path):
+def get_absolute_path(file_path):
     return (
-        path
-        if path.startswith("/")
-        else os.path.realpath(os.path.join(PYTEXT_HOME, path))
+        file_path
+        if os.path.isabs(file_path)
+        else os.path.realpath(os.path.join(PYTEXT_HOME, file_path))
     )
