5e4ab99ab104c555dc641ef07fa4afde8870e612
Steven Liu <stevenliu@fb.com>
2019-10-11 21:33:44 -0700

    Back out "DocNN as a Lazy Module, better constructor" (#1047)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/1047
    
    Original commit changeset: c1d54c2937ee
    
    Reviewed By: JiesiZhao077
    
    Differential Revision: D17891513
    
    fbshipit-source-id: 28e54c540b45114f8c2fddafd5b0095d41eb5a14

--- pytext/models/representations/docnn.py
+++ pytext/models/representations/docnn.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
-from typing import List
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from pytext.config import ConfigBase
 from pytext.config.module_config import CNNParams
-from pytext.utils import lazy
 
 from .representation_base import RepresentationBase
 
@@ -19,40 +17,19 @@ class DocNNRepresentation(RepresentationBase):
         dropout: float = 0.4
         cnn: CNNParams = CNNParams()
 
-    @classmethod
-    def from_config(cls, config: Config, embed_dim: int = None):
-        # Lazy so we don't need the embed_dim
-        return cls(config.cnn.kernel_num, config.cnn.kernel_sizes, config.dropout)
-
-    def __init__(
-        self,
-        kernel_num: int = 100,
-        kernel_sizes: List[int] = (3, 4),
-        dropout: float = 0.4,
-    ) -> None:
-        """
-        Args:
-            kernel_num: Number of feature maps for each Conv1d kernel
-            kernel_sizes: A Conv1d kernel will be created with the corresponding size
-                for each kernel in this list; the results will be concatenated together
-                to form the final representation
-            dropout: Dropout ratio applied to the final representation during training
-        """
-        nn.Module.__init__(self)
-        self.max_kernel = max(kernel_sizes)
+    def __init__(self, config: Config, embed_dim: int) -> None:
+        super().__init__(config)
+        self.max_kernel = max(config.cnn.kernel_sizes)
         self.convs = nn.ModuleList(
-            [lazy.Conv1d(kernel_num, K, padding=K) for K in kernel_sizes]
+            [
+                nn.Conv1d(embed_dim, config.cnn.kernel_num, K, padding=K)
+                for K in config.cnn.kernel_sizes
+            ]
         )
-        self.dropout = nn.Dropout(dropout)
-
-        self.representation_dim = len(kernel_sizes) * kernel_num
+        self.dropout = nn.Dropout(config.dropout)
+        self.representation_dim = len(config.cnn.kernel_sizes) * config.cnn.kernel_num
 
     def forward(self, embedded_tokens: torch.Tensor, *args) -> torch.Tensor:
-        """Accepts a token embedding of size
-            (batch, sequence_length, embedding)
-        and returns a representation of size
-            (batch, (len(kernel_sizes) * kernel_num))
-        """
         # embedded_tokens of size (N,W,D)
         rep = embedded_tokens
         # nn.Conv1d expects a tensor of dim (batch_size x embed_dim x seq_len)
