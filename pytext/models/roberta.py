a4157af0a123f697785930c4a88e2ac1d7aa5c3c
Eric Yu <chenyangyu@instagram.com>
2019-10-23 17:13:11 -0700

    PyTorch eager mode implementation (#1072)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/1072
    
    PyTorch eager mode implementation so that could apply quantization before torchscriptify
    
    Reviewed By: rutyrinott
    
    Differential Revision: D18064396
    
    fbshipit-source-id: 2df5bea2942a9d7fd1400fc8b2401514dc331845

--- pytext/models/roberta.py
+++ pytext/models/roberta.py
@@ -9,14 +9,37 @@ from pytext.data.bert_tensorizer import RoBERTaTensorizer
 from pytext.data.tensorizers import LabelTensorizer
 from pytext.models.bert_classification_models import NewBertModel
 from pytext.models.module import Module, create_module
+from pytext.models.representations.transformer import (
+    MultiheadSelfAttention,
+    SentenceEncoder,
+    Transformer,
+    TransformerLayer,
+)
 from pytext.models.representations.transformer_sentence_encoder_base import (
     TransformerSentenceEncoderBase,
 )
 from pytext.torchscript.tensorizer import ScriptRoBERTaTensorizer
+from torch.serialization import default_restore_location
 
 
-class RoBERTaEncoder(TransformerSentenceEncoderBase):
+class RoBERTaEncoderBase(TransformerSentenceEncoderBase):
+    __EXPANSIBLE__ = True
+
     class Config(TransformerSentenceEncoderBase.Config):
+        pass
+
+    def _encoder(self, inputs):
+        # NewBertModel expects the output as a tuple and grabs the first element
+        tokens, _, _ = inputs
+        full_representation = self.encoder(tokens)
+        sentence_rep = full_representation[:, 0, :]
+        return [full_representation], sentence_rep
+
+
+class RoBERTaEncoderJit(RoBERTaEncoderBase):
+    """A TorchScript RoBERTa implementation"""
+
+    class Config(RoBERTaEncoderBase.Config):
         pretrained_encoder: Module.Config = Module.Config(
             load_path=(
                 "manifold://pytext_training/tree/static/models/roberta_public.pt1"
@@ -29,12 +52,40 @@ class RoBERTaEncoder(TransformerSentenceEncoderBase):
         self.encoder = create_module(config.pretrained_encoder)
         self.representation_dim = self.encoder.encoder.token_embedding.weight.size(-1)
 
-    def _encoder(self, inputs):
-        # NewBertModel expects the output as a tuple and grabs the first element
-        tokens, _, _ = inputs
-        full_representation = self.encoder(tokens)
-        sentence_rep = full_representation[:, 0, :]
-        return [full_representation], sentence_rep
+
+class RoBERTaEncoder(RoBERTaEncoderBase):
+    """A PyTorch RoBERTa implementation"""
+
+    class Config(RoBERTaEncoderBase.Config):
+        num_encoder_layers: int = 12
+        num_attention_heads: int = 12
+        model_path: str = "manifold://pytext_training/tree/static/models/roberta_base_torch.pt"
+
+    def __init__(self, config: Config, output_encoded_layers: bool, **kwarg) -> None:
+        super().__init__(config, output_encoded_layers=output_encoded_layers)
+        # assert config.pretrained_encoder.load_path, "Load path cannot be empty."
+        self.encoder = SentenceEncoder(
+            transformer=Transformer(
+                embedding_dim=config.embedding_dim,
+                layers=[
+                    TransformerLayer(
+                        embedding_dim=config.embedding_dim,
+                        attention=MultiheadSelfAttention(
+                            config.embedding_dim, config.num_attention_heads
+                        ),
+                    )
+                    for _ in range(config.num_encoder_layers)
+                ],
+            )
+        )
+        roberta_state = torch.load(
+            config.model_path,
+            map_location=lambda s, l: default_restore_location(s, "cpu"),
+        )
+        self.encoder.load_roberta_state_dict(roberta_state["model"])
+        self.representation_dim = self.encoder.transformer.token_embedding.weight.size(
+            -1
+        )
 
 
 class ScriptRoBERTa(torch.jit.ScriptModule):
@@ -66,7 +117,7 @@ class RoBERTa(NewBertModel):
             labels: LabelTensorizer.Config = LabelTensorizer.Config()
 
         inputs: InputConfig = InputConfig()
-        encoder: RoBERTaEncoder.Config = RoBERTaEncoder.Config()
+        encoder: RoBERTaEncoderBase.Config = RoBERTaEncoderJit.Config()
 
     def torchscriptify(self, tensorizers, traced_model):
         """Using the traced model, create a ScriptModule which has a nicer API that
