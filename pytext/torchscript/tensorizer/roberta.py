538e881485d9944455471531c7e5c955cff41fe3
Chenyang Yu <chenyangyu@instagram.com>
2019-11-05 12:56:47 -0800

    refactor ScriptTensorizor to support both text and tokens input (#1096)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/1096
    
    refactor ScriptTensorizor to support both text and tokens input
    
    Reviewed By: hudeven
    
    Differential Revision: D18264320
    
    fbshipit-source-id: 6bc5bc13a4cd90f2f92c6eeb2c173b3d4121829a

--- pytext/torchscript/tensorizer/roberta.py
+++ pytext/torchscript/tensorizer/roberta.py
@@ -5,25 +5,76 @@ from typing import List, Tuple
 
 import torch
 
-from .bert import ScriptBERTTensorizer
+from .bert import ScriptBERTTensorizerBase
 
 
-class ScriptRoBERTaTensorizer(ScriptBERTTensorizer):
+class ScriptRoBERTaTensorizerBase(ScriptBERTTensorizerBase):
     @torch.jit.script_method
     def numberize(self, row: List[str]) -> Tuple[List[int], List[int], int]:
-        tokens: List[int] = []
+        """Convert raw inputs into token ids by doing vocab look-up. It will also
+        append bos & eos index into token ids if needed.
+
+        Args:
+            row: 1) a list of raw inputs, in most case it is a
+                single text or a pair of texts.
+                 2) a list of preprocced tokens, we could still
+                apply other operations (for example: bpe) on it.
+
+        Returns:
+            a list of token ids after doing vocab lookup and segment labels.
+        """
+        token_ids: List[int] = []
         segment_labels: List[int] = []
         seq_len: int = 0
+        per_sentence_tokens: List[List[Tuple[str, int, int]]] = self.tokenize(row)
 
-        for idx, text in enumerate(row):
-            token_ids: List[int] = self.vocab_lookup(
-                self.tokenizer.tokenize(text),
+        for idx, tokens in enumerate(per_sentence_tokens):
+            lookup_ids: List[int] = self.vocab_lookup(
+                tokens,
                 bos_idx=self.vocab.bos_idx,
                 eos_idx=self.vocab.eos_idx,
                 max_seq_len=self.max_seq_len,
             )[0]
-            tokens.extend(token_ids)
-            segment_labels.extend([idx] * len(token_ids))
-        seq_len = len(tokens)
+            token_ids.extend(lookup_ids)
+            segment_labels.extend([idx] * len(lookup_ids))
+        seq_len = len(token_ids)
 
-        return tokens, segment_labels, seq_len
+        return token_ids, segment_labels, seq_len
+
+
+class ScriptRoBERTaTensorizer(ScriptRoBERTaTensorizerBase):
+    @torch.jit.script_method
+    def tokenize(self, row: List[str]) -> List[List[Tuple[str, int, int]]]:
+        """Convert raw inputs into tokens.
+
+        Args:
+            row: a list of raw inputs, in most case it is a
+                single text or a pair of texts.
+
+        Returns:
+            a per sentence list of tokens which include token index.
+        """
+
+        per_sentence_tokens: List[List[Tuple[str, int, int]]] = []
+        for text in row:
+            per_sentence_tokens.append(self.tokenizer.tokenize(text))
+        return per_sentence_tokens
+
+
+class ScriptRoBERTaTokenTensorizer(ScriptRoBERTaTensorizerBase):
+    @torch.jit.script_method
+    def tokenize(self, row: List[str]) -> List[List[Tuple[str, int, int]]]:
+        """Convert raw inputs into tokens.
+
+        Args:
+            row: a list of raw inputs, in most case it is a
+                single text or a pair of texts.
+
+        Returns:
+            a per sentence list of tokens which include token index.
+        """
+
+        per_sentence_tokens: List[Tuple[str, int, int]] = []
+        for raw_token in row:
+            per_sentence_tokens.extend(self.tokenizer.tokenize(raw_token))
+        return [per_sentence_tokens]
