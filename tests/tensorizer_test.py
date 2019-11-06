e022a0860433f9fe703ee0f940e2854e93566bc6
Nisarg Shah <snisarg@gmail.com>
2019-11-06 11:57:42 -0800

    FB linter, quotes change.

--- tests/tensorizer_test.py
+++ tests/tensorizer_test.py
@@ -10,17 +10,17 @@ class SentencePieceTokenizerTest(unittest.TestCase):
     def test_tokenize(self):
         sentence = "Testing out sentencepiece"
         expected = [
-            '▁T',
-            'est',
-            'ing',
-            '▁out',
-            '▁sen',
-            't',
-            'ence',
-            'p',
-            'i',
-            'e',
-            'ce',
+            "▁T",
+            "est",
+            "ing",
+            "▁out",
+            "▁sen",
+            "t",
+            "ence",
+            "p",
+            "i",
+            "e",
+            "ce",
         ]
         sp_tokenizer = SentencePieceTokenizer.from_config(
             SentencePieceTokenizer.Config(
