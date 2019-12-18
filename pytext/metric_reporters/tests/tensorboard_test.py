631e07fd15c3b1011652d82571b0d386bc0eaf3d
Kshitiz Malik <kmalik2@fb.com>
2019-12-18 15:54:34 -0800

    Make tensorboard robust to NaN and Inf in model params (#1206)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/1206
    
    PyText FBLearner training runs with `tensorboard` will fail if the model has any `NaN` or `Inf` parameters during training with the following error:
    `ValueError: The histogram is empty, please file a bug report.`
    
    NaN/Inf params show up quite often in hyperparam sweep runs if a bad initial value of hyperparam is chosen.
    When this happens, the whole sweep can fail.
    
    Some examples just from the past two days:
    f156398891
    f156398480
    f156398264
    f156399047
    f156399125
    
    This diff:
    - Catches tensorboard exceptions, and prints an error to stderr. Note that `TensordboardChannel` already catches some exceptions while exporting the model
    - Adds a unit-test with a model that has `NaN` and `Inf` parameters, to verify no Tensorboard errors
    
    Reviewed By: psuzhanhy
    
    Differential Revision: D19048506
    
    fbshipit-source-id: e82ab3e73bced1e749396e7f571cbae22b3eef76

---
+++ pytext/metric_reporters/tests/tensorboard_test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+from unittest import TestCase
+
+from pytext.common.constants import Stage
+from pytext.metric_reporters.channel import TensorBoardChannel
+from torch import nn, optim
+
+
+class FCModelWithNanAndInfWts(nn.Module):
+    """ Simple FC model
+    """
+
+    def __init__(self):
+        super(FCModelWithNanAndInfWts, self).__init__()
+        self.fc1 = nn.Linear(10, 5)
+        self.fc2 = nn.Linear(5, 3)
+        self.fc1.weight.data.fill_(float("NaN"))
+        self.fc2.weight.data.fill_(float("Inf"))
+
+    def forward(self, x):
+        x = self.fc1(x)
+        return self.fc2(x)
+
+
+class TensorboardTest(TestCase):
+    def test_report_metrics_with_nan(self):
+        """ Check that tensorboard channel catches errors when model has
+            Inf or NaN weights
+        """
+        tensorboard_channel = TensorBoardChannel()
+        # create simple model and optimizers
+        model = FCModelWithNanAndInfWts()
+
+        optimizer = optim.SGD(model.parameters(), lr=0.1)
+        tensorboard_channel.report(
+            stage=Stage.TRAIN,
+            epoch=1,
+            metrics=0.0,
+            model_select_metric=0.0,
+            loss=1.0,
+            preds=[1],
+            targets=[1],
+            scores=[1],
+            context={},
+            meta={},
+            model=model,
+            optimizer=optimizer,
+        )
