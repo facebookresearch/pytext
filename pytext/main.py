7d38355e4d86afc80d2f29fbb1781a46c5c6ccd2
Jean Maillard <jeanm@fb.com>
2020-01-15 11:32:51 -0800

    Missing 'f' from f-string (#1229)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/1229
    
    F-string was missing `f`.
    
    Reviewed By: arbabu123
    
    Differential Revision: D19377335
    
    fbshipit-source-id: b779ae73ae370ca9e3be2eafd87a70c3e20b798f

--- pytext/main.py
+++ pytext/main.py
@@ -60,7 +60,7 @@ def train_model_distributed(config, metric_channels: Optional[List[Channel]]):
         or config.distributed_world_size <= torch.cuda.device_count()
     ), (
         f"Only {torch.cuda.device_count()} GPUs are available, "
-        "{config.distributed_world_size} GPUs were requested"
+        f"{config.distributed_world_size} GPUs were requested"
     )
 
     print(f"\n=== Starting training, World size is {config.distributed_world_size}")
