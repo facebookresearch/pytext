3455649c95592a5920bd8ca26f67f09703c6f450
Ted Wild <twild@fb.com>
2020-01-24 16:15:49 -0800

    Torchscript and open-source friendly implementation of sequence generation for
    Differential Revision: D19275449
    
    fbshipit-source-id: 89eacb525742a53b2288bf9b7080df36f4d0f067

---
+++ pytext/torchscript/seq2seq/beam_search.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
+
+from typing import List, Tuple
+
+import torch
+import torch.jit
+import torch.jit.quantized
+from torch import nn
+
+from .decoder import DecoderBatchedStepEnsemble
+from .encoder import EncoderEnsemble
+
+
+@torch.jit.script
+def get_first_decoder_step_input(
+    beam_size: int = 5, eos_token_id: int = 0, src_length: int = 1
+):
+    prev_tokens = torch.full([beam_size], eos_token_id, dtype=torch.long)
+    prev_scores = torch.full([beam_size], 1, dtype=torch.float)
+    prev_hypos = torch.full([beam_size], 0, dtype=torch.long)
+    attention_weights = torch.full([beam_size, src_length], 1, dtype=torch.float)
+    return prev_tokens, prev_scores, prev_hypos, attention_weights
+
+
+class BeamSearch_UTTERANCE_EXP(nn.Module):
+    def __init__(
+        self,
+        model_list,
+        tgt_dict_eos,
+        beam_size: int = 2,
+        quantize: bool = False,
+        record_attention: bool = False,
+    ):
+        super().__init__()
+        self.models = model_list
+        self.target_dict_eos = tgt_dict_eos
+        self.beam_size = beam_size
+        self.record_attention = record_attention
+
+        # Script the encoder model
+        encoder_ens = EncoderEnsemble(self.models, self.beam_size)
+        if quantize:
+            encoder_ens = torch.jit.quantized.quantize_linear_modules(encoder_ens)
+            encoder_ens = torch.jit.quantized.quantize_rnn_cell_modules(encoder_ens)
+
+        self.encoder_ens = torch.jit.script(encoder_ens)
+
+        # Script the decoder step
+        decoder_ens = DecoderBatchedStepEnsemble(
+            self.models, beam_size, record_attention=record_attention
+        )
+        if quantize:
+            decoder_ens = torch.jit.quantized.quantize_linear_modules(decoder_ens)
+            decoder_ens = torch.jit.quantized.quantize_rnn_cell_modules(decoder_ens)
+
+        self.decoder_ens = torch.jit.script(decoder_ens)
+
+    def forward(
+        self, src_tokens: Tuple[torch.Tensor], src_lengths: torch.Tensor, num_steps: int
+    ):
+        # Initialize incremental_states after every forward()
+        self.decoder_ens.reset_incremental_states()
+
+        # ARBABU: To do add an assert to make sure that batch size is 1
+        decoder_ip = self.encoder_ens(src_tokens, src_lengths)
+
+        (
+            prev_token,
+            prev_scores,
+            prev_hypos_indices,
+            attention_weights,
+        ) = get_first_decoder_step_input(
+            self.beam_size, self.target_dict_eos, src_lengths[0]
+        )
+
+        all_tokens_list = [prev_token]
+        all_scores_list = [prev_scores]
+        all_prev_indices_list = [prev_hypos_indices]
+        all_attentions_list: List[torch.Tensor] = []
+        if self.record_attention:
+            all_attentions_list.append(attention_weights)
+
+        for i in range(num_steps):
+            (
+                prev_token,
+                prev_scores,
+                prev_hypos_indices,
+                attention_weights,
+                decoder_ip,
+            ) = self.decoder_ens(prev_token, prev_scores, i + 1, decoder_ip)
+            all_tokens_list.append(prev_token)
+            all_scores_list.append(prev_scores)
+            all_prev_indices_list.append(prev_hypos_indices)
+            if self.record_attention:
+                all_attentions_list.append(attention_weights)
+
+        all_tokens = torch.stack(all_tokens_list)
+        all_scores = torch.stack(all_scores_list)
+        all_prev_indices = torch.stack(all_prev_indices_list)
+        if self.record_attention:
+            all_attn_weights = torch.stack(all_attentions_list)
+        else:
+            all_attn_weights = torch.zeros(
+                num_steps + 1, self.beam_size, src_tokens[0].size(1)
+            )
+
+        return all_tokens, all_scores, all_attn_weights, all_prev_indices
