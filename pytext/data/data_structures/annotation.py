aee73cfd8c3906d330da9989697679dd4e01a293
Ted Wild <twild@fb.com>
2020-02-12 10:35:35 -0800

    Land autogen RBC rules based on project spec
    Reviewed By: poojasethi
    
    Differential Revision: D19774391
    
    fbshipit-source-id: 1d0a6bdd564a5ed823780f83aa6a425d51405126

--- pytext/data/data_structures/annotation.py
+++ pytext/data/data_structures/annotation.py
@@ -9,6 +9,7 @@ CLOSE = "]"
 ESCAPE = "\\"
 INTENT_PREFIX = "IN:"
 SLOT_PREFIX = "SL:"
+OOD_TOKEN = "outOfDomain"
 COMBINATION_INTENT_LABEL = INTENT_PREFIX + "COMBINE"
 COMBINATION_SLOT_LABEL = SLOT_PREFIX + "COMBINE"
 SHIFT = "SHIFT"
@@ -107,6 +108,8 @@ class Annotation:
                             node: Union[Intent, Slot, Token] = Intent(word)
                         elif word.startswith(SLOT_PREFIX):
                             node = Slot(word)
+                        elif word == OOD_TOKEN:
+                            node = Intent(word)
                         elif accept_flat_intents_slots:
                             # Temporary, for compatibility with flat annotations that
                             # does not contain IN:, SL: prefixes. This assumes any child
