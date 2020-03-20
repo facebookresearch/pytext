93fd47d7d4c724c94e6d8c257c1b4d60da37a5ef
Arun Babu <arbabu@fb.com>
2020-03-20 14:36:54 -0700

    Add new fb beam search with fused operator (#1287)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/1287
    
    New fb beam search
    
    Fb_Seq2Seq will support the lightconv models + fused operator
    ScriptedNLG* will support lightconv + fused operator
    ScriptedDecoupled will support lightconv + fused operator
    
    Reviewed By: twwhatever
    
    Differential Revision: D20529564
    
    fbshipit-source-id: 992e78646513c81e6d95678f19fad95981babd78

--- pytext/torchscript/seq2seq/decoder.py
+++ pytext/torchscript/seq2seq/decoder.py
@@ -8,48 +8,6 @@ import torch.jit.quantized
 from torch import Tensor, nn
 
 
-@torch.jit.script
-def beam_search_aggregate_topk(
-    log_probs_per_model: List[torch.Tensor],
-    attn_weights_per_model: List[torch.Tensor],
-    prev_scores: torch.Tensor,
-    beam_size: int,
-    record_attention: bool,
-):
-    average_log_probs = torch.mean(
-        torch.cat(log_probs_per_model, dim=1), dim=1, keepdim=True
-    )
-
-    best_scores_k_by_k, best_tokens_k_by_k = torch.topk(
-        average_log_probs.squeeze(1), k=beam_size
-    )
-
-    prev_scores_k_by_k = prev_scores.view(-1, 1).expand(-1, beam_size)
-    total_scores_k_by_k = best_scores_k_by_k + prev_scores_k_by_k
-
-    # flatten to take top k over all (beam x beam) hypos
-    total_scores_flat = total_scores_k_by_k.view(-1)
-    best_tokens_flat = best_tokens_k_by_k.view(-1)
-
-    best_scores, best_indices = torch.topk(total_scores_flat, k=beam_size)
-
-    best_tokens = best_tokens_flat.index_select(dim=0, index=best_indices).view(-1)
-
-    # integer division to determine which input produced each successor
-    prev_hypos = best_indices / beam_size
-
-    if record_attention:
-        average_attn_weights = torch.mean(
-            torch.cat(attn_weights_per_model, dim=1), dim=1, keepdim=True
-        )
-        attention_weights = average_attn_weights.index_select(dim=0, index=prev_hypos)
-        attention_weights = attention_weights.squeeze_(1)
-    else:
-        attention_weights = torch.zeros(beam_size, attn_weights_per_model[0].size(2))
-
-    return (best_scores, best_tokens, prev_hypos, attention_weights)
-
-
 class DecoderBatchedStepEnsemble(nn.Module):
     """
     This method should have a common interface such that it can be called after
@@ -119,7 +77,7 @@ class DecoderBatchedStepEnsemble(nn.Module):
             best_tokens,
             prev_hypos,
             attention_weights,
-        ) = beam_search_aggregate_topk(
+        ) = self.beam_search_aggregate_topk(
             log_probs_per_model,
             attn_weights_per_model,
             prev_scores,
@@ -132,3 +90,48 @@ class DecoderBatchedStepEnsemble(nn.Module):
             model.decoder.reorder_incremental_state(incremental_state, prev_hypos)
 
         return best_tokens, best_scores, prev_hypos, attention_weights, decoder_ips
+
+    def beam_search_aggregate_topk(
+        self,
+        log_probs_per_model: List[torch.Tensor],
+        attn_weights_per_model: List[torch.Tensor],
+        prev_scores: torch.Tensor,
+        beam_size: int,
+        record_attention: bool,
+    ):
+        average_log_probs = torch.mean(
+            torch.cat(log_probs_per_model, dim=1), dim=1, keepdim=True
+        )
+
+        best_scores_k_by_k, best_tokens_k_by_k = torch.topk(
+            average_log_probs.squeeze(1), k=beam_size
+        )
+
+        prev_scores_k_by_k = prev_scores.view(-1, 1).expand(-1, beam_size)
+        total_scores_k_by_k = best_scores_k_by_k + prev_scores_k_by_k
+
+        # flatten to take top k over all (beam x beam) hypos
+        total_scores_flat = total_scores_k_by_k.view(-1)
+        best_tokens_flat = best_tokens_k_by_k.view(-1)
+
+        best_scores, best_indices = torch.topk(total_scores_flat, k=beam_size)
+
+        best_tokens = best_tokens_flat.index_select(dim=0, index=best_indices).view(-1)
+
+        # integer division to determine which input produced each successor
+        prev_hypos = best_indices / beam_size
+
+        if record_attention:
+            average_attn_weights = torch.mean(
+                torch.cat(attn_weights_per_model, dim=1), dim=1, keepdim=True
+            )
+            attention_weights = average_attn_weights.index_select(
+                dim=0, index=prev_hypos
+            )
+            attention_weights = attention_weights.squeeze_(1)
+        else:
+            attention_weights = torch.zeros(
+                beam_size, attn_weights_per_model[0].size(2)
+            )
+
+        return (best_scores, best_tokens, prev_hypos, attention_weights)
