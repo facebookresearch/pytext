25f90d2ac211514706639dc949364a0ae5dd4a8d
Steven Liu <stevenliu@fb.com>
2020-07-08 11:08:11 -0700

    adopt project-specific PathManager (#1394)
    Summary: Pull Request resolved: https://github.com/facebookresearch/pytext/pull/1394
    
    Reviewed By: liaimi
    
    Differential Revision: D22412077
    
    fbshipit-source-id: 1a625b5b233fd54aff164ab4db978cf84983b322

--- pytext/utils/file_io.py
+++ pytext/utils/file_io.py
@@ -4,7 +4,10 @@
 # keep PathManager here for more flexibility until PathManager becomes more mature
 # in case we want some hacks in PathManager, we can do it here without updating
 # the import everywhere in PyText
-from fvcore.common.file_io import HTTPURLHandler, PathManager  # noqa
+from fvcore.common.file_io import HTTPURLHandler, PathManagerBase  # noqa
+
+
+PathManager = PathManagerBase()
 
 
 def register_http_url_handler():
