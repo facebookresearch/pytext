332f6151cc83112f249d9a1e50ee0b7e2251c244
Steven Liu <stevenliu@fb.com>
2020-08-24 10:55:06 -0700

    add PyTextDataset
    Differential Revision: D22924158
    
    fbshipit-source-id: 5cac5b1210db3784fbaf49504fdd88d10b62f413

---
+++ pytext/contrib/pytext_lib/datasets/tsv_dataset.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+from typing import Any, Dict, List, Optional
+
+from pytext.contrib.pytext_lib.datasets.batchers import Batcher
+from pytext.data.sources.data_source import SafeFileWrapper
+from pytext.data.sources.tsv import TSV
+
+from ..transforms import Transform
+from .pytext_dataset import PyTextDataset
+
+
+class TsvDataset(PyTextDataset):
+    def __init__(
+        self,
+        path: str,
+        columns: List[Any] = None,
+        column_mapping: Optional[Dict[str, str]] = None,
+        delimiter: str = "\t",
+        batch_size: Optional[int] = None,
+        is_shuffle: bool = True,
+        transform: Optional[Transform] = None,
+        custom_batcher: Optional[Batcher] = None,
+        collate_fn=None,
+        chunk_size: int = 1000,
+        is_cycle: bool = False,
+        length: Optional[int] = None,
+        rank: int = 0,
+        num_workers: int = 1,
+    ):
+        columns = columns or ["text", "label"]
+        if column_mapping:
+            raise NotImplementedError("column mapping is not supported for tsv yet!")
+        self.file = SafeFileWrapper(path, encoding="utf-8", errors="replace")
+        tsv_iterator = TSV(self.file, field_names=columns, delimiter=delimiter)
+        super().__init__(
+            iterable=tsv_iterator,
+            batch_size=batch_size,
+            is_shuffle=is_shuffle,
+            transform=transform,
+            custom_batcher=custom_batcher,
+            collate_fn=collate_fn,
+            chunk_size=chunk_size,
+            is_cycle=is_cycle,
+            length=length,
+            rank=rank,
+            num_workers=num_workers,
+        )
--- pytext/contrib/pytext_lib/utils.py
+++ pytext/contrib/pytext_lib/utils.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from collections import defaultdict
+from typing import Any, Dict
 
 
-def rows_to_columnar(rows):
+def rows_to_columnar(rows) -> Dict[str, Any]:
     columnar = defaultdict(list)
     for row in rows:
         for column, value in row.items():
@@ -14,6 +15,22 @@ def rows_to_columnar(rows):
 def columnar_to_rows(columnar):
     rows = []
     for index in range(len(columnar.values()[0])):
-        for column, lists in columnar.items():
-            rows.append({column: lists[index]})
+        row = {column: lists[index] for column, lists in columnar.items()}
+        rows.append(row)
+    return rows
+
+
+def rows_to_columnar_tuple(rows):
+    columnar_tuple = tuple([] for _ in range(len(rows[0])))
+    for row in rows:
+        for i, item in enumerate(row):
+            columnar_tuple[i].append(item)
+    return columnar_tuple
+
+
+def columnar_tuple_to_rows(columnar_tuple):
+    rows = []
+    for index in range(len(columnar_tuple[0])):
+        row = [lists[index] for lists in columnar_tuple if lists]
+        rows.append(row)
     return rows
