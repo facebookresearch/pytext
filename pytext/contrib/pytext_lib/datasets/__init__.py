9d236811c2aa3acd70296bd8448f5e002d358878
Steven Liu <stevenliu@fb.com>
2020-08-24 10:55:06 -0700

    deprecate old transform and dataset prototypes
    Differential Revision: D23225050
    
    fbshipit-source-id: c8eb064b4716384e8cae31a9796aeab04dc875c6

--- pytext/contrib/pytext_lib/datasets/__init__.py
+++ pytext/contrib/pytext_lib/datasets/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reservedimport pytext_lib
 
-from .base_dataset import BaseDataset, PoolingBatcher
-from .tsv_dataset import TsvDataset
+from .base_dataset_deprecated import BaseDataset
+from .tsv_dataset_deprecated import TsvDataset
 
 
-__all__ = ["BaseDataset", "PoolingBatcher", "TsvDataset"]
+__all__ = ["BaseDataset", "TsvDataset"]
