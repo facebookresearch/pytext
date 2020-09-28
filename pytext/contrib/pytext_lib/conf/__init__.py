9aa96b44af7b5f57a3542a3d0c6f5b333132941a
Steven Liu <stevenliu@fb.com>
2020-09-28 09:46:08 -0700

    generic doc classification task with hydra
    Summary:
    we will provide 2 types of tasks.
    1) generic task, users can switch components(e.g. Model, Optimizer etc) by config. It's more flexible during experiments, but reduce the readability(components are found by hydra through "_target_")
    2) specific task(e.g. XlmrForDocClassificationTask) which has hard coded the components and only provides config for Hyper Params. It's easier to read and maintain.
    
    The development process:
    1. experiment: use generic task to sweep for the best model architecture
    2. go to production: create a specific task with the best architecture and defaults you found in 1. And TorchScript it
    
    Config structure:
      pytext_config:
        data_module:
        task:
          model:
          optim:
        trainer:
    
    Caveats:
    1. The concept of DataModule, LightningModule, PyText Task seems confusing. need some discussion on this.
    
    2. Union is not supported in Hydra/OmegaConf. The workaround is to use Inheritance and register the schema in ConfigStore. It's not very intuitive.
    
    3. ConfigStore and dataclasses could be split into multiple files, or use some automation, when we have more tasks to add
    
    Differential Revision: D23697911
    
    fbshipit-source-id: a14fa86f7057cdef25fff8474bc2cb05fc8d85fe

--- pytext/contrib/pytext_lib/conf/__init__.py
+++ pytext/contrib/pytext_lib/conf/__init__.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved#!/usr/bin/env python3
 from dataclasses import dataclass, field
-from typing import Any, List, Optional, Tuple
+from typing import Any, Dict, List, Optional, Tuple
 
 # @manual "//github/facebookresearch/hydra:hydra"
 from hydra.core.config_store import ConfigStore
 from omegaconf import MISSING
+from stl.lightning.conf.trainer import TrainerConf
 
 
 project_defaults = [
@@ -29,12 +30,18 @@ class DataConf:
 
 @dataclass
 class TransformConf:
+    _target_: str = "pytext.contrib.pytext_lib.transforms.fb_doc_transform.DocTransform"
     label_names: Optional[List[str]] = field(default_factory=lambda: ["True", "False"])
-    vocab_path: str = MISSING
+    vocab_path: Optional[str] = None
 
 
 @dataclass
 class ModelConf:
+    pass
+
+
+@dataclass
+class RobertaModelConf(ModelConf):
     model_path: Optional[str] = None
     dense_dim: Optional[int] = 0
     embedding_dim: int = 32
@@ -44,37 +51,38 @@ class ModelConf:
     num_encoder_layers: int = 1
     output_dropout: float = 0.4
     bias: bool = True
+    # TODO: @steven make Roberta model accept primitive atgs, then add _target_
+
+
+@dataclass
+class DocClassificationModelConf(ModelConf):
+    _target_: str = "pytext.contrib.pytext_lib.models.DocClassificationModel"
+    # word embedding config
+    pretrained_embeddings_path: str = MISSING
+    embedding_dim: int = MISSING
+    mlp_layer_dims: Optional[List[int]] = field(default_factory=list)
+    lowercase_tokens: bool = False
+    skip_header: bool = True
+    delimiter: str = " "
+    # DocNN config
+    kernel_num: int = 100
+    kernel_sizes: Optional[List[int]] = field(default_factory=list)
+    pooling_type: str = "max"
+    dropout: float = 0.4
+    # decoder config
+    dense_dim: int = 0
+    decoder_hidden_dims: Optional[List[int]] = field(default_factory=list)
+    out_dim: int = 2
 
 
 @dataclass
 class OptimConf:
+    _target_: str = "torch.optim.AdamW"
     lr: float = 1e-3
     betas: Tuple[float, float] = (0.9, 0.999)
     eps: float = 1e-8
     weight_decay: float = 0
     amsgrad: bool = False
-
-
-@dataclass
-class TrainerConf:
-    """Config for Lightning Trainer
-    See the usage of each arg in pytorch_lightning/trainer/trainer.py
-    """
-
-    max_epochs: int = 1
-    # `gpus` should be of type Optional[Union[List[int], str, int]]
-    # However, Hydra doesn't support Union yet, and flow type doesn't support Any
-    gpus: Optional[int] = None
-    distributed_backend: Optional[str] = None
-    num_nodes: int = 1
-    checkpoint_callback: bool = False
-    default_root_dir: Optional[str] = None
-    replace_sampler_ddp: bool = False
-    num_sanity_val_steps: int = 1
-    log_gpu_memory: Optional[str] = None
-    reload_dataloaders_every_epoch: bool = False
-    row_log_interval: int = 10
-    weights_summary: Optional[str] = "full"
 
 
 @dataclass
@@ -84,27 +92,94 @@ class ClassificationMetricReporterConf:
     )
 
 
+# TODO: @stevenliu merge to TaskConf
 @dataclass
 class DocClassificationConfig:
     # MISSING doesn't work with non "str" type in flow
     data: DataConf
     transform: TransformConf
-    model: ModelConf
+    model: RobertaModelConf
     optim: OptimConf
     metric_reporter: ClassificationMetricReporterConf = ClassificationMetricReporterConf()
     trainer: TrainerConf = TrainerConf()
     defaults: List[Any] = field(default_factory=lambda: project_defaults)
 
 
+@dataclass
+class DataModuleConf:
+    pass
+
+
+@dataclass
+class DocClassificationDataModuleConf(DataModuleConf):
+    _target_: str = "pytext.contrib.pytext_lib.data.datamodules.doc_classification.DocClassificationDataModule"
+    train_path: str = MISSING
+    val_path: str = MISSING
+    test_path: str = MISSING
+    columns: List[str] = field(default_factory=lambda: ["text", "label"])
+    column_mapping: Optional[Dict[str, str]] = None
+    delimiter: str = "\t"
+    batch_size: Optional[int] = None
+    is_shuffle: bool = True
+    chunk_size: int = 1000
+    is_cycle: bool = False
+    length: Optional[int] = None
+    transform: TransformConf = TransformConf()
+
+
+@dataclass
+class TaskConf:
+    _target_: str = "pytext.contrib.pytext_lib.tasks.fb_doc_classification_task.DocClassificationTask"
+    model: ModelConf = MISSING
+    optim: OptimConf = MISSING
+
+
+@dataclass
+class PyTextConf:
+    datamodule: DataModuleConf = MISSING
+    task: TaskConf = TaskConf()
+    trainer: TrainerConf = TrainerConf()
+    defaults: List[Any] = field(
+        default_factory=lambda: (
+            {"datamodule": "doc_classification"},
+            {"task/model": "doc_model_with_spm"},
+            {"task/optim": "fairseq_adam"},
+            {"trainer": "cpu"},
+        )
+    )
+
+
 cs = ConfigStore.instance()
 cs.store(group="data", name="sst2", node=DataConf)
 cs.store(group="data", name="sst2_dummy", node=DataConf)
 cs.store(group="transform", name="roberta", node=TransformConf)
-cs.store(group="model", name="xlmr", node=ModelConf)
-cs.store(group="model", name="xlmr_dummy", node=ModelConf)
-cs.store(group="optim", name="fairseq_adam", node=OptimConf)
-cs.store(group="trainer", name="cpu", node=TrainerConf)
-cs.store(group="trainer", name="single_gpu", node=TrainerConf)
-cs.store(group="trainer", name="multi_gpu", node=TrainerConf)
 
+cs.store(group="schema/task/model", name="xlmr", node=RobertaModelConf)
+cs.store(group="task/model", name="xlmr_base", node=RobertaModelConf)
+cs.store(group="task/model", name="xlmr_dummy", node=RobertaModelConf)
+
+cs.store(group="schema/task/model", name="doc_model", node=DocClassificationModelConf)
+cs.store(group="task/model", name="doc_model_dummy", node=DocClassificationModelConf)
+cs.store(group="task/model", name="doc_model_with_spm", node=DocClassificationModelConf)
+cs.store(group="task/model", name="doc_model_with_xlu", node=DocClassificationModelConf)
+
+cs.store(group="task/optim", name="adamw", node=OptimConf)
+cs.store(group="task/optim", name="fairseq_adam", node=OptimConf)
+
+# TODO: @stevenliu merge it to task_config
 cs.store(name="config", node=DocClassificationConfig)
+
+cs.store(
+    group="schema/datamodule",
+    name="doc_classification",
+    node=DocClassificationDataModuleConf,
+)
+cs.store(
+    group="datamodule", name="doc_classification", node=DocClassificationDataModuleConf
+)
+cs.store(
+    group="datamodule",
+    name="doc_classification_dummy",
+    node=DocClassificationDataModuleConf,
+)
+cs.store(name="pytext_config", node=PyTextConf)
---
+++ pytext/contrib/pytext_lib/data/datamodules/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+from .doc_classification import DocClassificationDataModule
+
+
+__all__ = ["DocClassificationDataModule"]
