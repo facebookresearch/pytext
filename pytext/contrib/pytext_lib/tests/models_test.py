b4506651d97a48458405f4b81dbe750a366979d3
Steven Liu <stevenliu@fb.com>
2020-09-28 16:57:09 -0700

    pass vocab to DocModel
    Summary: for some cases(e.g. using SPM tokenizer), the vocab is already built outside. We can pass the vocab to DocModel directly
    
    Reviewed By: snisarg
    
    Differential Revision: D23872785
    
    fbshipit-source-id: 17801523bf06f9a14e6679f4471f3b301120c6ad

--- pytext/contrib/pytext_lib/tests/models_test.py
+++ pytext/contrib/pytext_lib/tests/models_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+import os
 import unittest
 
 import torch.nn as nn
@@ -7,11 +8,12 @@ from pytext.contrib.pytext_lib import models
 from pytext.contrib.pytext_lib.models.intent_slot_model import (
     build_dumb_intent_slot_model,
 )
+from pytext.contrib.pytext_lib.transforms import VocabTransform
 
 
 class TestModels(unittest.TestCase):
     def setUp(self):
-        pass
+        self.base_dir = os.path.join(os.path.dirname(__file__), "data")
 
     def test_load_xlmr_dummy(self):
         model = models.xlmr_dummy_binary_doc_classifier(pretrained=False)
@@ -27,4 +29,21 @@ class TestModels(unittest.TestCase):
 
     def test_load_intent_slot(self):
         model = build_dumb_intent_slot_model()
+        assert isinstance(model, nn.Module)
+
+    def test_load_doc_model(self):
+        transform = VocabTransform(os.path.join(self.base_dir, "vocab_dummy"))
+        vocab = transform.vocab
+        model = models.DocClassificationModel(
+            pretrained_embeddings_path=os.path.join(
+                self.base_dir, "word_embedding_dummy"
+            ),
+            embedding_dim=300,
+            mlp_layer_dims=(2,),
+            skip_header=True,
+            kernel_num=1,
+            kernel_sizes=(3, 4, 5),
+            decoder_hidden_dims=(2,),
+            vocab=vocab,
+        )
         assert isinstance(model, nn.Module)
