1317d058ee91be6829ce76f9a45a3f991ca96488
Jiesi Zhao <jiesi@fb.com>
2020-09-29 10:39:53 -0700

    Use TorchText SPM in PyText (#1456)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/1456
    
    Create a open-source friendly `SpmTokenizerTransform` that depends on TorchText. Point roberta and xlmr task to the new default tokenizer.
    
    Reviewed By: hudeven
    
    Differential Revision: D23399772
    
    fbshipit-source-id: a9575d8525373305b19f40078c6d2373781fe4af

--- pytext/contrib/pytext_lib/transforms/transforms.py
+++ pytext/contrib/pytext_lib/transforms/transforms.py
@@ -4,10 +4,12 @@ from collections import defaultdict
 from typing import Any, Dict, List, Optional
 
 import torch.nn as nn
+from pytext.contrib.pytext_lib.resources import url
 from pytext.data.bert_tensorizer import build_fairseq_vocab
 from pytext.data.utils import BOS, EOS, MASK, PAD, UNK
 from pytext.torchscript.vocab import ScriptVocabulary
 from pytext.utils.file_io import PathManager
+from torchtext.data.functional import load_sp_model
 
 
 class IdentityTransform(nn.Module):
@@ -105,3 +107,25 @@ class TruncateTransform(nn.Module):
 
     def forward(self, token_ids: List[List[int]]) -> List[List[int]]:
         return [token_id[: self.max_seq_len] for token_id in token_ids]
+
+
+class SpmTokenizerTransform(nn.Module):
+    """
+    Uses TorchText SPM tokenizer
+    """
+
+    def __init__(self, sp_model_path: Optional[str] = None):
+        super().__init__()
+
+        # This default spm file path is a dummy link as we haven't published
+        # the file yet. Please provide your own spm file path when using
+        # this transform
+        sp_model_path = sp_model_path or url.URL[url.SP_MODEL]
+        local_path = PathManager.get_local_path(sp_model_path)
+        self.sp_model = load_sp_model(local_path)
+
+    def forward(self, texts: List[str]) -> List[List[str]]:
+        tokens: List[List[str]] = []
+        for text in texts:
+            tokens.append(self.sp_model.EncodeAsPieces(text))
+        return tokens
