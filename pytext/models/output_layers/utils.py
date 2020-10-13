d9aaa73948537972e69eb058c00611d3c1f5b681
Shrey Desai <shreyd@fb.com>
2020-10-13 09:12:55 -0700

    Move word indexing to helper function (#1482)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/1482
    
    Moves intent slot model word indexing to a helper function for maintainability
    
    Reviewed By: abhinavarora
    
    Differential Revision: D24174159
    
    fbshipit-source-id: 77ca04d6f2c2ed544b279a60645086cea982a4d4

--- pytext/models/output_layers/utils.py
+++ pytext/models/output_layers/utils.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
 from typing import List
 
 import torch
 from caffe2.python import core
+from torch import Tensor
 
 
 class OutputLayerUtils:
@@ -31,3 +33,20 @@ class OutputLayerUtils:
         for name, label_score in zip(label_names, label_scores):
             res.append(predict_net.Copy(label_score, "{}:{}".format(output_name, name)))
         return res
+
+
+def query_word_reprs(encoder_repr: Tensor, token_indices: Tensor) -> Tensor:
+    """
+    Given an encoder_repr (B x T_1 x H) and token_indices (B x T_2) where T_2 <= T_1,
+    collect embeddings from encoder_repr pertaining to indices in token_indices. In the
+    context of fine-tuning pre-trained encoders on sequence labeling, our goal is to
+    build token-level representations as opposed to subword-level represenatations
+    for alignment with other token-level cues, such as dictionary features. Currently,
+    a token representation is built by taking its first subword representation.
+    """
+
+    return torch.gather(
+        encoder_repr,
+        1,
+        token_indices.unsqueeze(2).expand(-1, -1, encoder_repr.size(-1)),
+    )
