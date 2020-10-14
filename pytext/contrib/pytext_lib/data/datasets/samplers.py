90e72d323737ea9b8d54df9630cee73e64adaac1
Jean Maillard <jeanm@fb.com>
2020-10-14 12:26:03 -0700

    Paged datasets
    Summary: Adds `PagedDataset`, a wrapper around an iterable which allows accessing it as a map-style dataset.
    
    Reviewed By: hudeven
    
    Differential Revision: D24279139
    
    fbshipit-source-id: 03f0ed2bd7e88b27747128cc6ce5f3c7a3c47aa1

--- pytext/contrib/pytext_lib/data/datasets/samplers.py
+++ pytext/contrib/pytext_lib/data/datasets/samplers.py
@@ -19,15 +19,15 @@ class SortedSampler(SequentialSampler):
         return iter(self.sorted_indices)
 
 
-class PooledBatchSampler(BatchSampler):
+class PagedBatchSampler(BatchSampler):
     """
-    Wraps an iterable to yield batches of indices from a sorted pool.
+    Wraps an iterable to yield batches of indices from a sorted page.
 
-    This sampler sequentially samples a pool of `num_batches_in_pool * batch_size` items
+    This sampler sequentially samples a page of `num_batches_in_page * batch_size` items
     from the base iterable, optionally sorts them according to a given key, and batches them.
-    Batches from a pool are returned in random order.
+    Batches from a page are returned in random order.
 
-    Increasing the size of the pool (larger `num_batches_in_pool`) means that the items in
+    Increasing the size of the page (larger `num_batches_in_page`) means that the items in
     the resulting batches will have more uniform length, but it also increases memory usage.
     """
 
@@ -37,30 +37,30 @@ class PooledBatchSampler(BatchSampler):
         batch_size: int,
         drop_last: bool,
         key: Optional[Callable] = None,
-        num_batches_in_pool: int = 100,
+        num_batches_in_page: int = 1024,
     ):
         sampler = SequentialSampler(data_source)
         super().__init__(sampler, batch_size, drop_last)
         self.data_source = data_source
         self.key = key
-        self.pool_sampler = BatchSampler(
+        self.page_sampler = BatchSampler(
             sampler=sampler,
-            batch_size=min(batch_size * num_batches_in_pool, len(sampler)),
+            batch_size=min(batch_size * num_batches_in_page, len(sampler)),
             drop_last=False,
         )
 
     def __iter__(self):
-        for pool_indices in self.pool_sampler:
+        for page_indices in self.page_sampler:
             if self.key is not None:
-                in_pool_sampler = SortedSampler(
-                    pool_indices, key=lambda i: self.key(self.data_source[i])
+                in_page_sampler = SortedSampler(
+                    page_indices, key=lambda i: self.key(self.data_source[i])
                 )
             else:
-                in_pool_sampler = SequentialSampler(pool_indices)
+                in_page_sampler = SequentialSampler(page_indices)
             batch_sampler = BatchSampler(
-                in_pool_sampler, self.batch_size, self.drop_last
+                in_page_sampler, self.batch_size, self.drop_last
             )
             batches = list(batch_sampler)
             random.shuffle(batches)
             for batch_indices in batches:
-                yield [pool_indices[i] for i in batch_indices]
+                yield [page_indices[i] for i in batch_indices]
