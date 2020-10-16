Fix backprop bug when using non-zero hard loss weight.
Summary:
Got `RuntimeError: grad can be implicitly created only for scalar outputs` on f224839642.

After some digging, found that we don't mean() the has_answer hard_loss so the final loss object becomes a tensor instead of scalar.

Also change cross_entropy loss to nll_loss per comment on Line 42 of loss.py

Reviewed By: stanvp

Differential Revision: D24349613

fbshipit-source-id: 4109b72a6bab3d53fa5b93e6010f1ce7dcd06dde

pytext/loss/loss.py
@@ -440,11 +440,11 @@ class KLDivergenceCELoss(Loss):
         soft_loss *= self.t ** 2  # See https://arxiv.org/pdf/1503.02531.pdf
         hard_loss = 0.0
         if self.hard_weight > 0.0:
-            hard_loss = F.cross_entropy(
-                logits,
+            hard_loss = F.nll_loss(
+                F.log_softmax(logits, 1, dtype=torch.float32),
                 hard_targets,
-                reduction="mean" if reduce else "none",
                 weight=self.weight,
+                reduction="mean" if reduce else "none",
             )
 
         return (