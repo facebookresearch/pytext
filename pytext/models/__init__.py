bb9c89a170c2f34fe1a8c1f005010931d043c794
Xiangge Meng <xianggemeng@fb.com>
2020-10-20 20:20:27 -0700

    Move Fluent2DataSource into pytext folder
    Summary: as titled. when registering pytext workflow, Fluent2DataSource is not registered yet, so input schema doesn't take Fluent2DataSource type.
    
    Reviewed By: liaimi
    
    Differential Revision: D24416547
    
    fbshipit-source-id: 5db618944411c8f3c6e29fe145381efc9fc86c9e

--- pytext/models/__init__.py
+++ pytext/models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from .model import BaseModel, Model
+from .two_tower_classification_model import TwoTowerClassificationModel
 
 
-__all__ = ["Model", "BaseModel"]
+__all__ = ["Model", "BaseModel", "TwoTowerClassificationModel"]
