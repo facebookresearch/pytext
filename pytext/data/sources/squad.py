d734e58fd56236a643bd21b58f33e9e24d52bd13
Debojeet Chatterjee <debo@fb.com>
2020-10-29 23:40:24 -0700

    Organize and clean up SquadDataSource code.
    Summary: As title.
    
    Reviewed By: stanvp
    
    Differential Revision: D24627341
    
    fbshipit-source-id: 2ec5898ab57f278ac8cd01a957a776bd1a04c650

--- pytext/data/sources/squad.py
+++ pytext/data/sources/squad.py
@@ -69,192 +69,6 @@ def _split_document(
     return pieces
 
 
-def process_squad_json(fname, ignore_impossible, max_character_length, min_overlap):
-    if not fname:
-        return
-    if not PathManager.exists(fname):
-        print(f"{fname} does not exist. Not unflattening.")
-        return
-
-    with PathManager.open(fname) as infile:
-        dump = json.load(infile)
-
-    id = 0
-    for article in dump["data"]:
-        for paragraph in article["paragraphs"]:
-            doc = paragraph["context"]
-            for question in paragraph["qas"]:
-                has_answer = not question.get("is_impossible", False)
-                answers = (
-                    question["answers"] if has_answer else question["plausible_answers"]
-                )
-                question = question["question"]
-                answer_texts = [answer["text"] for answer in answers]
-                answer_starts = [int(answer["answer_start"]) for answer in answers]
-                for piece_dict in _split_document(
-                    id,
-                    doc,
-                    question,
-                    answer_texts,
-                    answer_starts,
-                    has_answer,
-                    ignore_impossible,
-                    max_character_length,
-                    min_overlap,
-                ):
-                    yield piece_dict
-                id += 1
-
-
-def process_squad_tsv(
-    fname, ignore_impossible, max_character_length, min_overlap, delimiter, quoted
-):
-    if not fname:
-        print(f"Empty file name!")
-        return
-
-    field_names = ["doc", "question", "answers", "answer_starts", "has_answer"]
-    tsv_file = SafeFileWrapper(
-        get_absolute_path(fname), encoding="utf-8", errors="replace"
-    )
-    tsv = TSV(
-        tsv_file,
-        field_names=field_names,
-        delimiter=delimiter,
-        quoted=quoted,
-        drop_incomplete_rows=True,
-    )
-
-    for id, row in enumerate(tsv):
-        parts = (row[f] for f in field_names)
-        doc, question, answers, answer_starts, has_answer = parts
-        answers = json.loads(answers)
-        answer_starts = json.loads(answer_starts)
-
-        for piece_dict in _split_document(
-            id,
-            doc,
-            question,
-            answers,
-            answer_starts,
-            has_answer == "True",
-            ignore_impossible,
-            max_character_length,
-            min_overlap,
-        ):
-            yield piece_dict
-
-
-def process_squad_tsv_for_kd(
-    fname, ignore_impossible, max_character_length, min_overlap, delimiter, quoted
-):
-    if not fname:
-        print(f"Empty file name!")
-        return
-
-    field_names = [
-        "id1",
-        "doc",
-        "question",
-        "answers",
-        "answer_starts",
-        "has_answer",
-        "id2",
-        "start_logits",
-        "end_logits",
-        "has_answer_logits",
-        "pad_mask",
-        "segment_labels",
-    ]
-    tsv_file = SafeFileWrapper(
-        get_absolute_path(fname), encoding="utf-8", errors="replace"
-    )
-    tsv = TSV(
-        tsv_file,
-        field_names=field_names,
-        delimiter=delimiter,
-        quoted=quoted,
-        drop_incomplete_rows=True,
-    )
-
-    for id, row in enumerate(tsv):
-        parts = (row[f] for f in field_names)
-        # All model output for KD are dumped using json serialization.
-        (
-            id1,
-            doc,
-            question,
-            answers,
-            answer_starts,
-            has_answer,
-            id2,
-            start_logits,
-            end_logits,
-            has_answer_logits,
-            pad_mask,
-            segment_labels,
-        ) = (json.loads(s) for s in parts)
-        for piece_dict in _split_document(
-            id,
-            doc,
-            question,
-            answers,
-            answer_starts,
-            has_answer == "True",
-            ignore_impossible,
-            max_character_length,
-            min_overlap,
-        ):
-            piece_dict.update(
-                {
-                    "start_logits": start_logits,
-                    "end_logits": end_logits,
-                    "has_answer_logits": has_answer_logits,
-                    "pad_mask": pad_mask,
-                    "segment_labels": segment_labels,
-                }
-            )
-            yield piece_dict
-
-
-def process_squad(
-    fname,
-    ignore_impossible,
-    max_character_length,
-    min_overlap=0.1,
-    delimiter="\t",
-    quoted=False,
-    is_kd=False,
-):
-    if fname.split(".")[-1] == "json":
-        return process_squad_json(
-            fname=fname,
-            ignore_impossible=ignore_impossible,
-            max_character_length=max_character_length,
-            min_overlap=min_overlap,
-        )
-    else:
-        return (
-            process_squad_tsv(
-                fname=fname,
-                ignore_impossible=ignore_impossible,
-                max_character_length=max_character_length,
-                min_overlap=min_overlap,
-                delimiter=delimiter,
-                quoted=quoted,
-            )
-            if not is_kd
-            else process_squad_tsv_for_kd(
-                fname=fname,
-                ignore_impossible=ignore_impossible,
-                max_character_length=max_character_length,
-                min_overlap=min_overlap,
-                delimiter=delimiter,
-                quoted=quoted,
-            )
-        )
-
-
 class SquadDataSource(DataSource):
     """
     Download data from https://rajpurkar.github.io/SQuAD-explorer/
@@ -319,14 +133,84 @@ class SquadDataSource(DataSource):
         self.quoted = quoted
 
     def process_file(self, fname):
-        return process_squad(
-            fname=fname,
-            ignore_impossible=self.ignore_impossible,
-            max_character_length=self.max_character_length,
-            min_overlap=self.min_overlap,
+        # Pick which method to use based on extension.
+        if fname.split(".")[-1] == "json":
+            return self.process_squad_json(fname=fname)
+        else:
+            return self.process_squad_tsv(fname=fname)
+
+    def process_squad_json(self, fname):
+        if not fname:
+            return
+        if not PathManager.exists(fname):
+            print(f"{fname} does not exist. Not unflattening.")
+            return
+        with PathManager.open(fname) as infile:
+            dump = json.load(infile)
+
+        id = 0
+        for article in dump["data"]:
+            for paragraph in article["paragraphs"]:
+                doc = paragraph["context"]
+                for question in paragraph["qas"]:
+                    has_answer = not question.get("is_impossible", False)
+                    answers = (
+                        question["answers"]
+                        if has_answer
+                        else question["plausible_answers"]
+                    )
+                    question = question["question"]
+                    answer_texts = [answer["text"] for answer in answers]
+                    answer_starts = [int(answer["answer_start"]) for answer in answers]
+                    for piece_dict in _split_document(
+                        id,
+                        doc,
+                        question,
+                        answer_texts,
+                        answer_starts,
+                        has_answer,
+                        self.ignore_impossible,
+                        self.max_character_length,
+                        self.min_overlap,
+                    ):
+                        yield piece_dict
+                    id += 1
+
+    def process_squad_tsv(self, fname):
+        if not fname:
+            print("Empty file name!")
+            return
+
+        field_names = ["doc", "question", "answers", "answer_starts", "has_answer"]
+        tsv_file = SafeFileWrapper(
+            get_absolute_path(fname), encoding="utf-8", errors="replace"
+        )
+        tsv = TSV(
+            tsv_file,
+            field_names=field_names,
             delimiter=self.delimiter,
             quoted=self.quoted,
+            drop_incomplete_rows=True,
         )
+
+        for id, row in enumerate(tsv):
+            parts = (row[f] for f in field_names)
+            doc, question, answers, answer_starts, has_answer = parts
+            answers = json.loads(answers)
+            answer_starts = json.loads(answer_starts)
+
+            for piece_dict in _split_document(
+                id,
+                doc,
+                question,
+                answers,
+                answer_starts,
+                has_answer == "True",
+                self.ignore_impossible,
+                self.max_character_length,
+                self.min_overlap,
+            ):
+                yield piece_dict
 
     @generator_property
     def train(self):
@@ -366,13 +250,71 @@ class SquadDataSourceForKD(SquadDataSource):
         }
         super().__init__(**kwargs)
 
-    def process_file(self, fname):
-        return process_squad(
-            fname=fname,
-            ignore_impossible=self.ignore_impossible,
-            max_character_length=self.max_character_length,
-            min_overlap=self.min_overlap,
+    def process_squad_tsv(self, fname):
+        # Process SQUAD TSV for KD
+        if not fname:
+            print("Empty file name!")
+            return
+        field_names = [
+            "id1",
+            "doc",
+            "question",
+            "answers",
+            "answer_starts",
+            "has_answer",
+            "id2",
+            "start_logits",
+            "end_logits",
+            "has_answer_logits",
+            "pad_mask",
+            "segment_labels",
+        ]
+        tsv_file = SafeFileWrapper(
+            get_absolute_path(fname), encoding="utf-8", errors="replace"
+        )
+        tsv = TSV(
+            tsv_file,
+            field_names=field_names,
             delimiter=self.delimiter,
             quoted=self.quoted,
-            is_kd=True,
+            drop_incomplete_rows=True,
         )
+
+        for id, row in enumerate(tsv):
+            parts = (row[f] for f in field_names)
+            # All model output for KD are dumped using json serialization.
+            (
+                id1,
+                doc,
+                question,
+                answers,
+                answer_starts,
+                has_answer,
+                id2,
+                start_logits,
+                end_logits,
+                has_answer_logits,
+                pad_mask,
+                segment_labels,
+            ) = (json.loads(s) for s in parts)
+            for piece_dict in _split_document(
+                id,
+                doc,
+                question,
+                answers,
+                answer_starts,
+                has_answer == "True",
+                self.ignore_impossible,
+                self.max_character_length,
+                self.min_overlap,
+            ):
+                piece_dict.update(
+                    {
+                        "start_logits": start_logits,
+                        "end_logits": end_logits,
+                        "has_answer_logits": has_answer_logits,
+                        "pad_mask": pad_mask,
+                        "segment_labels": segment_labels,
+                    }
+                )
+                yield piece_dict
