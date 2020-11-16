5ab47ef4fd207fa73b1b4f26b2c7ebe2165a5cb7
Eric Yu <chenyangyu@instagram.com>
2020-11-16 11:51:24 -0800

    move multiray wrapper to pytext/fb (#1539)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/1539
    
    move multiray wrapper to pytext/fb, we should not expose this in OSS
    
    Reviewed By: gunchu
    
    Differential Revision: D24998192
    
    fbshipit-source-id: 070fde59cd6725d29749fc440156f6284aa5615b

--- pytext/torchscript/quantization_schema_enum.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved#!/usr/bin/env python3
-
-from typing import Dict
-
-
-# Keep in sync with QuantizationType enum in textray_contants.thrift
-# We can't directly use the thrift enum because TorchScript doesn't support it
-QUANTIZATION_SCHEMA: Dict[str, int] = {"NONE": 1, "INT1_QUANTIZATION": 2}
