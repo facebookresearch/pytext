dae78febafcd07b3f6ce505964297bfaae1f2a43
Michael Gschwind <mikekg@fb.com>
2021-01-19 21:55:42 -0800

    add model2trace_path
    Summary:
    add model2trace_path
    Kludgy adjustment based on empirical observation about relationship between model and trace hierarchy
    
    Differential Revision: D25968658
    
    fbshipit-source-id: 289fb22d0ad1a94ffa3f86b13a05b22abef7863d

--- pytext/task/accelerator_lowering.py
+++ pytext/task/accelerator_lowering.py
@@ -84,6 +84,7 @@ def lower_modules_to_accelerator(
     if hasattr(model, "encoder") and isinstance(model.encoder, RoBERTaEncoder):
         backend = "NNPI"
         submod_path, compilation_spec_dict = accelerator.get_modules(model, backend)[0]
+        submod_path = accelerator.model2trace_path(submod_path)
         embedding_dim = model.encoder.encoder.transformer.token_embedding.embedding_dim
         spec = torch_glow.CompilationSpec()
         spec.get_settings().set_glow_backend(backend)
@@ -109,7 +110,7 @@ def lower_modules_to_accelerator(
 
         trace = torch_glow.to_glow_selective(
             trace,
-            {f"model.{submod_path}": spec},
+            {submod_path: spec},
             inplace=False,
         )
 
