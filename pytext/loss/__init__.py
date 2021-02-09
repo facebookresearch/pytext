92d85f788a366b2ee8781abe9633f13c6f33da28
Shrey Desai <shreyd@fb.com>
2021-02-09 04:01:12 -0800

    Open-source non-autoregressive optimization (#1601)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/1601
    
    Open-sourcing several optimization modules, including regularization functions, label smoothing, and non-autoregressive losses.
    
    Reviewed By: arbabu123
    
    Differential Revision: D26188165
    
    fbshipit-source-id: 2044306db439158d3ad3bf3b74437b074b52e2db

--- pytext/loss/__init__.py
+++ pytext/loss/__init__.py
@@ -18,7 +18,15 @@ from .loss import (
     NLLLoss,
     PairwiseRankingLoss,
     SourceType,
+    MaxMarginLoss,
 )
+from .regularized_loss import (
+    LabelSmoothingLoss,
+    SamplewiseLabelSmoothingLoss,
+    NARSequenceLoss,
+    NARSamplewiseSequenceLoss,
+)
+from .regularizer import UniformRegularizer, EntropyRegularizer, AdaptiveRegularizer
 
 
 __all__ = [
@@ -38,4 +46,12 @@ __all__ = [
     "PairwiseRankingLoss",
     "LabelSmoothedCrossEntropyLoss",
     "SourceType",
+    "LabelSmoothingLoss",
+    "SamplewiseLabelSmoothingLoss",
+    "MaxMarginLoss",
+    "NARSequenceLoss",
+    "NARSamplewiseSequenceLoss",
+    "UniformRegularizer",
+    "EntropyRegularizer",
+    "AdaptiveRegularizer",
 ]
