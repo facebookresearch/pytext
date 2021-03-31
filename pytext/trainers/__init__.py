4cbc5b2c0380fd1d63db8eaead8edd7c3f5f0970
Anchit Gupta <anchit@fb.com>
2021-03-31 13:18:05 -0700

    fp16 grad compression trainer (#1661)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/pytext/pull/1661
    
    1) Adds fp16 gradient compression option to PyText trainer. Config adapters were causing too many test failures which were hard to debug so i just add a new trainer
    
    Reviewed By: AkshatSh
    
    Differential Revision: D27435035
    
    fbshipit-source-id: ac8b3b97dbd826f72f209e7cd09a0446d76df5b5

--- pytext/trainers/__init__.py
+++ pytext/trainers/__init__.py
@@ -2,7 +2,7 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 from .ensemble_trainer import EnsembleTrainer
 from .hogwild_trainer import HogwildTrainer, HogwildTrainer_Deprecated
-from .trainer import TaskTrainer, Trainer
+from .trainer import TaskTrainer, Trainer, FP16GradsTrainer
 from .training_state import TrainingState
 
 
@@ -13,4 +13,5 @@ __all__ = [
     "HogwildTrainer",
     "HogwildTrainer_Deprecated",
     "TaskTrainer",
+    "FP16GradsTrainer",
 ]
