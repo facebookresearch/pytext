df9fb805982734c25ddfea0b9cf95ecce5fe0b5a
Vanessa Feng <vanessafeng@fb.com>
2021-10-21 06:40:03 -0700

    Equal population quantize in textray
    Summary:
    - Implemented `equal population` quantization scheme in `textray`
    - Quantization boundaries are obtained using notebook N1229395
    - Experiment result comparing `equal space` and `equal population` quantization on 2-4 bits: https://fburl.com/mlhub/ypsvq5ni
    
    Reviewed By: rutyrinott
    
    Differential Revision: D31695052
    
    fbshipit-source-id: 354409b67c4f54887da70688c57db398f4128c18

--- pytext/config/config_adapter.py
+++ pytext/config/config_adapter.py
@@ -1098,6 +1098,20 @@ def v39_to_v40(json_config):
     return json_config
 
 
+@register_down_grade_adapter(from_version=41)
+def v41_to_v40(json_config):
+    for v in get_json_config_iterator(json_config, "encoder"):
+        if "uniform_quantization" in v:
+            del v["uniform_quantization"]
+    return json_config
+
+
+@register_adapter(from_version=40)
+def v409_to_v41(json_config):
+    # New config field was added with backwards-compatible default value
+    return json_config
+
+
 def get_name_from_options(export_config):
     """
     Reverse engineer which model is which based on recognized
